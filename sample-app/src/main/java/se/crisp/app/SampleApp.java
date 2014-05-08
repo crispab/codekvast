6560bcb3e659ca66cb1c144d599a5a65560d8fcc
ollehallin <UIY5GJtg>
2014-05-08 15:23:26 +0200

    Started working on isolating dependencies. The main application's classpath shall not be contaminated with duck-agent stuff, unless absolutely necessary.

--- sample-app/src/main/java/se/crisp/app/SampleApp.java
+++ sample-app/src/main/java/se/crisp/app/SampleApp.java
@@ -6,8 +6,23 @@ package se.crisp.app;
 public class SampleApp {
 
     public static void main(String[] args) {
-        System.out.printf("Hello, World! from %s%n", SampleApp.class.getName());
+        System.out.printf("Hello, World! from %s%n%n", SampleApp.class.getName());
         new Bar().m1();
+
+        tryToLoadClass("duck.spike.DuckAgent", true);
+        tryToLoadClass("org.aspectj.weaver.loadtime.Agent", true);
+        tryToLoadClass("org.reflections.Reflections", false);
+    }
+
+    private static void tryToLoadClass(String className, boolean expectedResult) {
+        try {
+            Class.forName(className);
+            String result = expectedResult ? "unavoidable" : "BAD!";
+            System.out.printf("%s can load class %s, which is %s.%n", SampleApp.class.getName(), className, result);
+        } catch (ClassNotFoundException e) {
+            String result = expectedResult ? "an ERROR" : "desirable";
+            System.out.printf("%s cannot load class %s, which is %s.%n", SampleApp.class.getName(), className, result);
+        }
     }
 
     public String meaningOfLife() {
