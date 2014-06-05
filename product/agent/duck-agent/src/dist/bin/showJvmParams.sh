6e9fb0ff39c2f5160c6b5a502e9d2a28b15f041e
Olle Hallin <olle.hallin@hit.se>
2014-06-05 13:34:29 +0200

    Debugged the installations

---
+++ product/agent/duck-agent/src/dist/bin/showJvmParams.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd $(dirname $0)/..
+DUCK_HOME=$PWD
+ENDORSED=$DUCK_HOME/endorsed
+SENSOR=$(find $ENDORSED -name *.jar)
+CONFIG=$DUCK_HOME/conf/duck.properties
+echo -javaagent:$SENSOR=$CONFIG -Djava.endorsed.dirs=$ENDORSED
