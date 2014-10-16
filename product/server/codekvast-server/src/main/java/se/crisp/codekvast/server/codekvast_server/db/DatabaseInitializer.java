4c1bba025866b204c42e2b9143b2050865cb20ef
Olle Hallin <olle.hallin@hit.se>
2014-10-16 11:11:31 +0200

    Added support for an embedded H2 database

--- product/server/codekvast-server/src/main/java/se/crisp/codekvast/server/codekvast_server/db/DatabaseInitializer.java
+++ product/server/codekvast-server/src/main/java/se/crisp/codekvast/server/codekvast_server/db/DatabaseInitializer.java
@@ -1,14 +1,15 @@
 package se.crisp.codekvast.server.codekvast_server.db;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
-
-/*
- No JDBC support for now
 import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-*/
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Initializes the database.
@@ -20,37 +21,35 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 @Configuration
 @Slf4j
 public class DatabaseInitializer {
-    public final String MIGRATION_LOCATION = getClass().getPackage().getName() + ".migration";
+    public static final String MIGRATION_LOCATION = DatabaseInitializer.class.getPackage().getName() + ".migration";
 
-    /*
-    * Disabled for now
     @Bean(initMethod = "migrate")
     public Flyway flyway(DataSource dataSource) throws SQLException {
         log.info("Migrating database at {}", dataSource.getConnection().getMetaData().getURL());
         Flyway flyway = new Flyway();
         flyway.setDataSource(dataSource);
         flyway.setLocations(MIGRATION_LOCATION);
+        flyway.migrate();
         return flyway;
     }
-    */
 
     /**
      * Override the default JdbcTemplate created by Spring Boot, to make sure that Flyway.migrate() has run first-
-     @Bean
-     @DependsOn("flyway")
-     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-     log.debug("Creates a JdbcTemplate");
-     return new JdbcTemplate(dataSource);
-     }
      */
+    @Bean
+    @DependsOn("flyway")
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        log.debug("Creates a JdbcTemplate");
+        return new JdbcTemplate(dataSource);
+    }
 
     /**
      * Override the default NamedParameterJdbcTemplate created by Spring Boot, to make sure that Flyway.migrate() has run first-
-     @Bean
-     @DependsOn("flyway")
-     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-     log.debug("Creates a NamedParameterJdbcTemplate");
-     return new NamedParameterJdbcTemplate(dataSource);
-     }
      */
+    @Bean
+    @DependsOn("flyway")
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        log.debug("Creates a NamedParameterJdbcTemplate");
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
 }
