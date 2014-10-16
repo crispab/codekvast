646c4919f89fb6412946ebdac5d96b786fa1914d
Olle Hallin <olle.hallin@hit.se>
2014-10-16 16:01:22 +0200

    Added Spring security (works now!)

--- product/server/codekvast-server/src/main/java/se/crisp/codekvast/server/codekvast_server/config/SecurityConfiguration.java
+++ product/server/codekvast-server/src/main/java/se/crisp/codekvast/server/codekvast_server/config/SecurityConfiguration.java
@@ -1,13 +1,9 @@
 package se.crisp.codekvast.server.codekvast_server.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -16,23 +12,18 @@ import javax.sql.DataSource;
  * @author Olle Hallin
  */
 @Configuration
-@EnableWebMvcSecurity
+// @EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Inject
     public void configureGlobal(DataSource dataSource, AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("0000").roles("USER");
+        // auth.inMemoryAuthentication().withUser("user").password("0000").roles("USER");
 
         auth.jdbcAuthentication().dataSource(dataSource)
             .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
             .authoritiesByUsernameQuery("SELECT users.username, users_roles.role FROM users_roles, users " +
-                                                " WHERE users_roles.userId = users.id AND users.username = ?");
-    }
-
-    @Bean
-    @DependsOn("flyway")
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+                                                " WHERE users_roles.userId = users.id AND users.username = ?")
+            .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     /*
