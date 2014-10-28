cc3c07c66e011e5f396cb4e0c9532e899b14b2ff
Olle Hallin <olle.hallin@hit.se>
2014-10-28 23:59:25 +0100

    Now minimizes signatures before writing to disk.

--- product/agent/util/src/test/java/se/crisp/codekvast/agent/util/SignatureUtilsTest.java
+++ product/agent/util/src/test/java/se/crisp/codekvast/agent/util/SignatureUtilsTest.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class SignatureUtilsTest {
@@ -16,6 +18,8 @@ public class SignatureUtilsTest {
     public void testMinimizeSignatures() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(SIGNATURES1)));
         String sig;
+        int longestMinimized = 0;
+        int longestSignature = 0;
         while ((sig = reader.readLine()) != null) {
             if (sig.trim().startsWith("#") || sig.trim().isEmpty()) {
                 continue;
@@ -23,9 +27,15 @@ public class SignatureUtilsTest {
             if (sig.contains("--------")) {
                 break;
             }
+
             String minimized = SignatureUtils.minimizeSignature(sig);
+
             assertTrue(minimized, minimized.length() < sig.length());
+            longestSignature = Math.max(longestSignature, sig.length());
+            longestMinimized = Math.max(longestMinimized, minimized.length());
         }
+        assertThat(longestSignature, is(1480));
+        assertThat(longestMinimized, is(1468));
     }
 
 }
