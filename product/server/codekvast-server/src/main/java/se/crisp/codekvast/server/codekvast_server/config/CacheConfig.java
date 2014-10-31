2f3900791f9f637f124d49d4c8758c902a1dfaa1
Olle Hallin <olle.hallin@hit.se>
2014-10-31 21:31:00 +0100

    Renamed main classes from *Main to *Application. Added support for @Cacheable (not yet working).

---
+++ product/server/codekvast-server/src/main/java/se/crisp/codekvast/server/codekvast_server/config/CacheConfig.java
@@ -0,0 +1,21 @@
+package se.crisp.codekvast.server.codekvast_server.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Olle Hallin
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        // TODO: make caching work...
+        return new ConcurrentMapCacheManager();
+    }
+}
