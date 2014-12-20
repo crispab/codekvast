3bd86bdb946d8f56855074c91b28717ef29354bb
Olle Hallin <olle.hallin@hit.se>
2014-12-20 18:26:04 +0100

    Improved javadocs.

--- product/support/websupport/src/main/java/se/crisp/codekvast/support/web/config/WebServerConfig.java
+++ product/support/websupport/src/main/java/se/crisp/codekvast/support/web/config/WebServerConfig.java
@@ -1,7 +1,6 @@
 package se.crisp.codekvast.support.web.config;
 
 import com.planetj.servlet.filter.compression.CompressingFilter;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
@@ -15,28 +14,23 @@ import java.io.File;
 
 /**
  * Configures the embedded web server.
- * It expects the following parameters in the Spring context:
- * <ul>
- *     <li>server.ssl.ports: a comma-separated list of port numbers. If empty, port 8443 is assumed.</li>
- * </ul>
+ * If it finds a keystore in a well-known location, add an https connector on port 8443.
+ * If the environment property "server.port" is something other than 8080 then the SSL port is chosen so that the difference is maintained.
+ * Example: server.port=8090 results in the SSL port 8453.
  *
  * @author Olle Hallin
  */
 @Configuration
-@Slf4j
 public class WebServerConfig {
 
-    /**
-     * If it finds a keystore in a well-known location, add an https connector on port 8443.
-     * If the http port is something other than 8080 then the SSL port is chosen so that the difference is maintained.
-     * Example: server.port=8090 results in the SSL port 8453.
-     */
+    public static final String KEYSTORE_PATH = "/etc/ssl/certs/java/star.crisp.se.jks";
+
     @Bean
     public EmbeddedServletContainerFactory servletContainer(Environment environment) {
         Integer serverPort = environment.getProperty("server.port", Integer.class, 8080);
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
 
-        File keystore = new File("/etc/ssl/certs/java/star.crisp.se.jks");
+        File keystore = new File(KEYSTORE_PATH);
         if (keystore.canRead()) {
             tomcat.addAdditionalTomcatConnectors(createSslConnector(keystore, computeSslPort(serverPort)));
         }
