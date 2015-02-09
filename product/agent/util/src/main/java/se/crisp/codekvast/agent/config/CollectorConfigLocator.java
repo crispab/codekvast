ea4f5ea92d10d7b0045007cdd9541bbd2b70351b
Olle Hallin <olle.hallin@hit.se>
2015-02-09 20:54:08 +0100

    Upgraded dependencies. Added CollectorConfigLocator (not yet in use).

---
+++ product/agent/util/src/main/java/se/crisp/codekvast/agent/config/CollectorConfigLocator.java
@@ -0,0 +1,102 @@
+package se.crisp.codekvast.agent.config;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * This class locates the file to feed into {@link CollectorConfig#parseCollectorConfig(java.net.URI, String...)} .
+ * <p/>
+ * It does this by checking a number of locations, stopping as soon as a file with the correct name is found.
+ *
+ * @author olle.hallin@crisp.se
+ */
+public class CollectorConfigLocator {
+
+    public static final String ENVVAR_CONFIG = "CODEKVAST_CONFIG";
+    public static final String ENVVAR_HOME = "CODEKVAST_HOME";
+    public static final String SYSPROP_CONFIG = "codekvast.configuration";
+    public static final String SYSPROP_HOME = "codekvast.home";
+    private static final String SYSPROP_CATALINA_HOME = "catalina.home";
+
+    private CollectorConfigLocator() {
+    }
+
+    public static URI locateConfig(PrintStream out) {
+        File file = tryLocation(out, System.getenv(ENVVAR_CONFIG));
+        if (file != null) {
+            return file.toURI();
+        }
+
+        file = tryLocation(out, System.getProperty(SYSPROP_CONFIG));
+        if (file != null) {
+            return file.toURI();
+        }
+
+        file = tryLocation(out, constructLocation(System.getenv(ENVVAR_HOME), "conf"));
+        if (file != null) {
+            return file.toURI();
+        }
+
+        file = tryLocation(out, constructLocation(System.getProperty(SYSPROP_HOME), "conf"));
+        if (file != null) {
+            return file.toURI();
+        }
+
+        file = tryLocation(out, constructLocation(System.getProperty(SYSPROP_CATALINA_HOME), "conf"));
+        if (file != null) {
+            return file.toURI();
+        }
+
+        file = tryLocation(out, constructLocation(getCollectorHome(), "conf"));
+        if (file != null) {
+            return file.toURI();
+        }
+        throw new IllegalArgumentException("Cannot find codekvast.conf");
+    }
+
+    private static String constructLocation(String home, String subdirectory) {
+        return home == null ? null : new File(home, subdirectory).getAbsolutePath();
+    }
+
+    private static File tryLocation(PrintStream out, String location) {
+        if (location == null) {
+            return null;
+        }
+
+        File file = new File(location);
+        if (file.isFile()) {
+            out.println("Found " + file);
+            return file;
+        }
+
+        file = new File(location, "codekvast-collector.conf");
+        if (file.canRead()) {
+            out.println("Found " + file);
+            return file;
+        }
+
+        file = new File(location, "codekvast.conf");
+        if (file.canRead()) {
+            out.println("Found " + file);
+            return file;
+        }
+
+        out.println("No codekvast.conf in " + location);
+        return null;
+    }
+
+    private static String getCollectorHome() {
+        try {
+            File myJar = new File(CollectorConfigLocator.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            File home = myJar.getParentFile();
+            if (home.getName().endsWith("/lib")) {
+                home = home.getParentFile();
+            }
+            return home.getAbsolutePath();
+        } catch (URISyntaxException e) {
+        }
+        return null;
+    }
+}
---
+++ product/agent/util/src/test/java/se/crisp/codekvast/agent/config/CollectorConfigLocatorTest.java
@@ -0,0 +1,50 @@
+package se.crisp.codekvast.agent.config;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static se.crisp.codekvast.agent.config.CollectorConfigLocator.SYSPROP_CONFIG;
+import static se.crisp.codekvast.agent.config.CollectorConfigLocator.SYSPROP_HOME;
+
+public class CollectorConfigLocatorTest {
+
+    @Before
+    public void beforeTest() throws Exception {
+        System.clearProperty(SYSPROP_CONFIG);
+        System.clearProperty(SYSPROP_HOME);
+    }
+
+    @Test
+    public void valid_file() throws Exception {
+        System.setProperty(SYSPROP_CONFIG, "src/test/resources/codekvast1.conf");
+        assertThat(CollectorConfigLocator.locateConfig(System.out), not(nullValue()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalid_file() throws Exception {
+        System.setProperty(SYSPROP_CONFIG, "src/test/resources/codekvast1.conf-FOOBAR");
+        CollectorConfigLocator.locateConfig(System.out);
+    }
+
+    @Test
+    public void valid_conf_directory() throws Exception {
+        System.setProperty(SYSPROP_CONFIG, "src/test/resources/collectorConfigLocatorTest/conf");
+        assertThat(CollectorConfigLocator.locateConfig(System.out), not(nullValue()));
+    }
+
+    @Test
+    public void valid_home() throws Exception {
+        System.setProperty(SYSPROP_HOME, "src/test/resources/collectorConfigLocatorTest");
+        assertThat(CollectorConfigLocator.locateConfig(System.out), not(nullValue()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void no_hints() throws Exception {
+        assertThat(CollectorConfigLocator.locateConfig(System.out), not(nullValue()));
+    }
+
+
+}
