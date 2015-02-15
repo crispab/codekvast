eef131bd059d020e4bb914dca05dd8844b499fac
Olle Hallin <olle.hallin@hit.se>
2015-02-15 14:45:31 +0100

    Added codekvast.multi-tenant=false

--- product/server/codekvast-server/src/main/java/se/crisp/codekvast/server/codekvast_server/controller/WebController.java
+++ product/server/codekvast-server/src/main/java/se/crisp/codekvast/server/codekvast_server/controller/WebController.java
@@ -1,7 +1,11 @@
 package se.crisp.codekvast.server.codekvast_server.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.inject.Inject;
 
 /**
  * @author olle.hallin@crisp.se
@@ -9,13 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController extends AbstractThymeleafController {
 
+    @Inject
+    @Value("${codekvast.multi-tenant}")
+    private boolean multiTenant;
+
     @RequestMapping({"/", "/index"})
     public String index() {
         return "index";
     }
 
     @RequestMapping({"/login"})
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("multiTenant", multiTenant);
+        if (!multiTenant) {
+            model.addAttribute("username", "guest");
+            model.addAttribute("password", "0000");
+        }
         return "login";
     }
 
