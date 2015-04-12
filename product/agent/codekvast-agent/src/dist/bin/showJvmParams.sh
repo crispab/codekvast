83f979c0bcc11a8db25ff7e18fee936a570905a9
Olle Hallin <olle.hallin@hit.se>
2015-04-12 13:02:21 +0200

    Improved UserManual

--- product/agent/codekvast-agent/src/dist/bin/showJvmParams.sh
+++ product/agent/codekvast-agent/src/dist/bin/showJvmParams.sh
@@ -4,4 +4,4 @@ CODEKVAST_HOME=$PWD
 COLLECTOR=$(find ${CODEKVAST_HOME}/javaagents -name '*collector*.jar')
 ASPECTJWEAVER=$(find ${CODEKVAST_HOME}/javaagents -name '*aspectjweaver*.jar')
 
-echo CODEKVAST_JVM_OPTS="-Dcodekvast.home=$CODEKVAST_HOME -javaagent:$CODEKVAST_HOME/javaagents/$(basename $COLLECTOR) -javaagent:$CODEKVAST_HOME/javaagents/$(basename $ASPECTJWEAVER)"
+echo JVM_OPTS="-Dcodekvast.home=$CODEKVAST_HOME -javaagent:$CODEKVAST_HOME/javaagents/$(basename $COLLECTOR) -javaagent:$CODEKVAST_HOME/javaagents/$(basename $ASPECTJWEAVER)"
