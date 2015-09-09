aa131d433f0503dc9bdb625870c63b1b4f50ce6d
ollehallin <olle.hallin@crisp.se>
2015-09-10 00:00:44 +0200

    Corrected JavaDocs

--- product/agent/util/src/main/java/se/crisp/codekvast/agent/io/DataDumper.java
+++ product/agent/util/src/main/java/se/crisp/codekvast/agent/io/DataDumper.java
@@ -12,17 +12,17 @@ public interface DataDumper {
     /**
      * Make preparations for dumping data, such as creating output directories.
      *
-     * @return If the preparation was successful.
+     * @return true iff the preparation was successful.
      */
     boolean prepareForDump();
 
     /**
-     * Dump the data
+     * Dump the data.
      *
-     * @param jvm                              The JVM data
-     * @param dumpCount                        The dump counter
-     * @param recordingIntervalStartedAtMillis When the recording of these invocations started
-     * @param invocations                      The set of invocations.
+     * @param jvm                              The JVM data.
+     * @param dumpCount                        The dump counter.
+     * @param recordingIntervalStartedAtMillis When the recording of these invocations started.
+     * @param invocations                      The set of invocations to dump.
      */
     void dumpData(Jvm jvm, int dumpCount, long recordingIntervalStartedAtMillis, Set<String> invocations);
 }
