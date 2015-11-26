77560d02389b21a63091ef6bf03255ecf4aaa392
Olle Hallin <olle.hallin@crisp.se>
2015-11-26 14:46:32 +0100

    Added confidence to methods view.

--- product/warehouse/src/main/resources/database/migration/V3_0__CreateView_MethodInvocations.sql
+++ product/warehouse/src/main/resources/database/migration/V3_0__CreateView_MethodInvocations.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE SQL SECURITY INVOKER VIEW MethodInvocations1 AS
     m.signature                                                              AS Method,
     a.name                                                                   AS Application,
     a.version                                                                AS Version,
-    IF(i.invokedAtMillis = 0, NULL, FROM_UNIXTIME(i.invokedAtMillis / 1000)) AS InvokedAt
+    IF(i.invokedAtMillis = 0, NULL, FROM_UNIXTIME(i.invokedAtMillis / 1000)) AS InvokedAt,
+    i.confidence                                                             AS Confidence
   FROM invocations i INNER JOIN methods m ON i.methodId = m.id
     INNER JOIN applications a ON i.applicationId = a.id;
