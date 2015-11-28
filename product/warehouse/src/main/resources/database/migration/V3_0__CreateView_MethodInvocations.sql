0fc47e88f5b332f048dd416b6822ec70d161879e
ollehallin <olle.hallin@crisp.se>
2015-11-28 17:42:09 +0100

    Improved the view MethodInvocations1

--- product/warehouse/src/main/resources/database/migration/V3_0__CreateView_MethodInvocations.sql
+++ product/warehouse/src/main/resources/database/migration/V3_0__CreateView_MethodInvocations.sql
@@ -1,9 +1,14 @@
 CREATE OR REPLACE SQL SECURITY INVOKER VIEW MethodInvocations1 AS
   SELECT
-    m.signature                                                              AS Method,
-    a.name                                                                   AS Application,
-    a.version                                                                AS Version,
-    IF(i.invokedAtMillis = 0, NULL, FROM_UNIXTIME(i.invokedAtMillis / 1000)) AS InvokedAt,
-    i.confidence                                                             AS Confidence
-  FROM invocations i INNER JOIN methods m ON i.methodId = m.id
-    INNER JOIN applications a ON i.applicationId = a.id;
+    DISTINCT
+    m.signature                                                                        AS Method,
+    GROUP_CONCAT(DISTINCT a.name SEPARATOR ', ')                                       AS Applications,
+    GROUP_CONCAT(DISTINCT a.version SEPARATOR ', ')                                    AS Versions,
+    GROUP_CONCAT(DISTINCT j.environment SEPARATOR ', ')                                AS Environments,
+    IF(max(i.invokedAtMillis) = 0, NULL, FROM_UNIXTIME(MAX(i.invokedAtMillis / 1000))) AS LastInvokedAt,
+    MIN(j.startedAt)                                                                   AS CollectedSince
+  FROM invocations i
+    INNER JOIN applications a ON i.applicationId = a.id
+    INNER JOIN methods m ON i.methodId = m.id
+    INNER JOIN jvms j ON i.jvmId = j.id
+  GROUP BY m.signature;
