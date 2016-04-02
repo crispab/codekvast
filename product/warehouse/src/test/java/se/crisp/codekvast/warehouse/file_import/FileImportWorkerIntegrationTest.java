754fd379586c78339d761bb4000898ab8d194ebb
Olle Hallin <olle.hallin@hit.se>
2016-04-02 16:07:56 +0200

    Adapted to Spring Boot conventions. Replaced application code for retrying failed database connects with docker-compose config.

---
+++ product/warehouse/src/test/java/se/crisp/codekvast/warehouse/file_import/FileImportWorkerIntegrationTest.java
@@ -0,0 +1,21 @@
+package se.crisp.codekvast.warehouse.file_import;
+
+import org.junit.Test;
+
+/**
+ * @author olle.hallin@crisp.se
+ */
+// @RunWith(SpringJUnit4ClassRunner.class)
+// @SpringApplicationConfiguration(classes = CodekvastWarehouse.class)
+// @IntegrationTest({
+//         "spring.datasource.url=jdbc:h2:mem:integrationTest",
+//         "codekvast.importPathPollIntervalSeconds=82400"
+// })
+public class FileImportWorkerIntegrationTest {
+
+    @Test
+    public void shouldImport_V1_ZipFile() throws Exception {
+
+
+    }
+}
