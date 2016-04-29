a31d5e5fa6b6e4ed2ff502da285c8bcaba5b57ee
Olle Hallin <olle.hallin@hit.se>
2016-04-29 17:30:03 +0200

    Added some info REST endpoints

--- product/warehouse/src/main/java/se/crisp/codekvast/warehouse/api/ApiController.java
+++ product/warehouse/src/main/java/se/crisp/codekvast/warehouse/api/ApiController.java
@@ -10,11 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import se.crisp.codekvast.warehouse.api.model.GetMethodsRequest1;
 import se.crisp.codekvast.warehouse.api.model.GetMethodsResponse1;
 import se.crisp.codekvast.warehouse.api.model.MethodDescriptor1;
+import se.crisp.codekvast.warehouse.bootstrap.CodekvastSettings;
 
 import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static se.crisp.codekvast.warehouse.api.ApiService.DEFAULT_MAX_RESULTS_STR;
@@ -25,15 +30,17 @@ import static se.crisp.codekvast.warehouse.api.ApiService.DEFAULT_MAX_RESULTS_ST
  * @author olle.hallin@crisp.se
  */
 @RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(value = "/api/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Slf4j
 public class ApiController {
 
     private final ApiService apiService;
+    private final CodekvastSettings settings;
 
     @Inject
-    public ApiController(ApiService apiService) {
+    public ApiController(ApiService apiService, CodekvastSettings settings) {
         this.apiService = apiService;
+        this.settings = settings;
     }
 
     @ExceptionHandler
@@ -45,7 +52,7 @@ public class ApiController {
         return ResponseEntity.badRequest().body(violations.toString());
     }
 
-    @RequestMapping(method = GET, value = "/api/v1/methods")
+    @RequestMapping(method = GET, value = "/v1/methods")
     public GetMethodsResponse1 getMethods1(@RequestParam(value = "signature", defaultValue = "%") String signature,
                                            @RequestParam(name = "maxResults", defaultValue = DEFAULT_MAX_RESULTS_STR) Integer maxResults) {
         return doGetMethods(signature, maxResults);
@@ -67,6 +74,27 @@ public class ApiController {
                                                           .build();
         log.debug("Response: {}", response);
         return response;
+    }
+
+    @RequestMapping(method = GET, value = "/instant")
+    public Instant getInstant() {
+        return Instant.now();
+    }
+
+    @RequestMapping(method = GET, value = "/localDateTime")
+    public LocalDateTime getLocalDateTime() {
+        return LocalDateTime.now();
+    }
+
+    @RequestMapping(method = GET, value = "/localDateTime/iso")
+    public String getLocalDateTimeString(Locale locale) {
+        DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME.withLocale(locale);
+        return LocalDateTime.now().format(dtf);
+    }
+
+    @RequestMapping(method = GET, value = "/version")
+    public CodekvastSettings getVersion() {
+        return settings;
     }
 
 }
