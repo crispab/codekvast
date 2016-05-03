752e08177b89cd3d53373885c86b88bb925dd549
Olle Hallin <olle.hallin@hit.se>
2016-05-03 18:46:11 +0200

    Introduced server-side rendering of webapp config.

--- product/warehouse/src/main/webapp/app/app.component.ts
+++ product/warehouse/src/main/webapp/app/app.component.ts
@@ -1,11 +1,17 @@
-import {Component} from "angular2/core";
+import {Component} from 'angular2/core';
+import {window} from 'angular2/src/facade/browser';
 
 @Component({
     selector: 'codekvast-warehouse',
-    templateUrl: 'templates/app.component.html' 
+    templateUrl: 'templates/app.component.html'
 })
 export class AppComponent {
 
-    private now:Date = new Date()
+    api: String;
+    now:Date = new Date();
+
+    constructor() {
+        this.api = window['CODEKVAST_API'];
+    }
 
 }
