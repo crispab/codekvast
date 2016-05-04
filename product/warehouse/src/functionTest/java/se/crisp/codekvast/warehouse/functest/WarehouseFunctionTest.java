768d73bd743dc03c0520b8e6d367c16bec683161
Olle Hallin <olle.hallin@hit.se>
2016-05-04 22:36:10 +0200

    Made warehouse endpoint available to function test.

--- product/warehouse/src/functionTest/java/se/crisp/codekvast/warehouse/functest/WarehouseFunctionTest.java
+++ product/warehouse/src/functionTest/java/se/crisp/codekvast/warehouse/functest/WarehouseFunctionTest.java
@@ -2,11 +2,15 @@ package se.crisp.codekvast.warehouse.functest;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.assertThat;
+
 public class WarehouseFunctionTest {
 
+    private final String warehouseEndpoint = System.getProperty("codekvast.warehouse.endpoint");
+
     @Test
-    public void placeholder() throws Exception {
-
-
+    public void should_have_warehouse_endpoint() throws Exception {
+        assertThat(warehouseEndpoint, startsWith("0.0.0.0:"));
     }
 }
