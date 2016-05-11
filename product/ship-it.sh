14bd605a3994bf8cbb99a17f24660e9e6842b0b5
Olle Hallin <olle.hallin@crisp.se>
2016-05-11 15:51:39 +0200

    WIP: replacing Gradle task shipIt with Bash script

--- product/ship-it.sh
+++ product/ship-it.sh
@@ -6,8 +6,22 @@ if [ $(git status --short | wc -l) -gt 0 ]; then
     # exit 1
 fi
 
-if [ $(git status --short --branch | grep -i '\[after ' | wc -l) -gt 0 ]; then
+if [ $(git status --short --branch | grep -i '\[ahead ' | wc -l) -gt 0 ]; then
     echo "The Git workspace is not pushed to origin. Git status:"
     git status --short --branch
-    exit 2
+    # exit 2
 fi
+
+if [ $(git status --short --branch | grep -i '\[behind ' | wc -l) -gt 0 ]; then
+    echo "The Git workspace is not pulled from origin. Git status:"
+    git status --short --branch
+    # exit 3
+fi
+
+declare GRADLEW=$(dirname $0)/../gradlew
+
+echo "Cleaning workspace..."
+$GRADLEW :product:clean
+
+echo "Building product..."
+$GRADLEW :product:build
