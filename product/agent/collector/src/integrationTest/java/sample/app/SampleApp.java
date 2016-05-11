815025c7f28901dba17ddf12640be6fa0fd08197
Olle Hallin <olle.hallin@crisp.se>
2016-05-11 15:40:40 +0200

    Added explicit constructor to SampleApp

--- product/agent/collector/src/integrationTest/java/sample/app/SampleApp.java
+++ product/agent/collector/src/integrationTest/java/sample/app/SampleApp.java
@@ -3,7 +3,13 @@ package sample.app;
 /**
  * @author olle.hallin@crisp.se
  */
+@SuppressWarnings("ALL")
 public class SampleApp {
+    private final int dummy;
+
+    public SampleApp() {
+        dummy = 17;
+    }
 
     public int add(int p1, int p2) {
         return p1 + p2;
