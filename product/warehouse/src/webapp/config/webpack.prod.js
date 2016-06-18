662080f9e9ac096cce32f8c86f2ff75e6809b518
Olle Hallin <olle.hallin@hit.se>
2016-06-18 17:15:20 +0200

    Corrected bug in webpack.prod.js

--- product/warehouse/src/webapp/config/webpack.prod.js
+++ product/warehouse/src/webapp/config/webpack.prod.js
@@ -1,10 +1,10 @@
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
-
-const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = webpackMerge(commonConfig, {
     devtool: 'source-map',
