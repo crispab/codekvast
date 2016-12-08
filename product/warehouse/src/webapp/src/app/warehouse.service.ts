6c4e72e4a748200ef2282a2ffd27cb047119864f
Olle Hallin <olle.hallin@crisp.se>
2016-12-08 23:09:34 +0100

    Optimized import

--- product/warehouse/src/webapp/src/app/warehouse.service.ts
+++ product/warehouse/src/webapp/src/app/warehouse.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Headers, Response, RequestOptionsArgs} from '@angular/http';
+import {Http, Headers, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import {ConfigService} from './config.service';
 import {MethodData} from './model/MethodData';
