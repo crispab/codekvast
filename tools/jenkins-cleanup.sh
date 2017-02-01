4e3913069994318e7257a6f43192a8489ee30f0d
Olle Hallin <olle.hallin@crisp.se>
2017-02-01 23:49:34 +0100

    Jenkinsfile: fixed ugly errors in jenkins-cleanup.sh

--- tools/jenkins-cleanup.sh
+++ tools/jenkins-cleanup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 declare IMAGE_NAME=crisp/codekvast-warehouse
-declare count=$(docker images --quiet ${IMAGE_NAME} | wc -l)
-if [ ${count} -gt 0 ]; then
-    echo "Attempt to remove $count $IMAGE_NAME images from the local Docker registry"
-    docker rmi --force $(docker images --quiet ${IMAGE_NAME}) || true
-    docker rmi --force $(docker images --quiet ${IMAGE_NAME}) || true
-fi
+for pass in $(seq 3); do
+    declare count=$(docker images --quiet ${IMAGE_NAME} | wc -l)
+    if [ ${count} -gt 0 ]; then
+        echo -e "Attempting to remove\n$(docker images ${IMAGE_NAME})"
+        docker rmi --force $(docker images --quiet ${IMAGE_NAME}) || true
+    fi
+done
