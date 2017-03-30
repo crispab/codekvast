61bb7d9903e0dace4d68c598446106598d7b9dbd
Olle Hallin <olle.hallin@crisp.se>
2017-03-30 22:34:07 +0200

    Implemented sorting in MethodsComponent

--- product/warehouse/src/webapp/src/app/methods.component.spec.ts
+++ product/warehouse/src/webapp/src/app/methods.component.spec.ts
@@ -5,11 +5,25 @@ import {MethodsComponent} from './methods.component';
 import {AppModule} from './app.module';
 import {WarehouseService} from './warehouse.service';
 import {ConfigService} from './config.service';
+import {MethodData} from './model/MethodData';
 
 let component: MethodsComponent;
 let fixture: ComponentFixture<MethodsComponent>;
 let signatureDE: DebugElement;
 let signatureElement: HTMLElement;
+
+const mockData: MethodData = {
+    methods: [
+        {
+            signature: 'sig2',
+            lastInvokedAtMillis: 2
+        }, {
+            signature: 'sig1',
+            lastInvokedAtMillis: 1
+        }
+    ]
+} as MethodData;
+
 
 describe('MethodsComponent', () => {
     beforeEach(() => {
@@ -19,9 +33,16 @@ describe('MethodsComponent', () => {
         TestBed.configureTestingModule({
             imports: [AppModule],
             providers: [
-                {provide: ConfigService, useValue: {}},
-                {provide: WarehouseService, useValue: warehouseServiceStub}
-            ] });
+                {
+                    provide: ConfigService,
+                    useValue: {}
+                },
+                {
+                    provide: WarehouseService,
+                    useValue: warehouseServiceStub
+                }
+            ]
+        });
 
         fixture = TestBed.createComponent(MethodsComponent);
 
@@ -42,4 +63,52 @@ describe('MethodsComponent', () => {
         fixture.detectChanges();
         expect(signatureElement.textContent).toBe('New Signature');
     });
+
+    it('should initially sort by signature', () => {
+        fixture.detectChanges();
+        expect(component.sortColumn).toBe('signature');
+    });
+
+    it('should initially sort ascending', () => {
+        fixture.detectChanges();
+        expect(component.sortAscending).toBe(true);
+    });
+
+    it('sortedMethods() should return null when null data', () => {
+        expect(component.sortedMethods()).toBeNull();
+    });
+
+    it('sortedMethods() should return null when empty data', () => {
+        component.data = new MethodData();
+        expect(component.sortedMethods()).toBeNull();
+    });
+
+    it('sortedMethods() should handle sort by signature ascending', () => {
+        component.data = mockData;
+        component.sortBySignature();
+        component.sortAscending = true;
+        expect(component.sortedMethods()[0].signature).toBe('sig1');
+    });
+
+    it('sortedMethods() should handle sort by signature descending', () => {
+        component.data = mockData;
+        component.sortBySignature();
+        component.sortAscending = false;
+        expect(component.sortedMethods()[0].signature).toBe('sig2');
+    });
+
+    it('sortedMethods() should handle sort by age ascending', () => {
+        component.data = mockData;
+        component.sortByAge();
+        component.sortAscending = true;
+        expect(component.sortedMethods()[0].lastInvokedAtMillis).toBe(1);
+    });
+
+    it('sortedMethods() should handle sort by age descending', () => {
+        component.data = mockData;
+        component.sortByAge();
+        component.sortAscending = false;
+        expect(component.sortedMethods()[0].lastInvokedAtMillis).toBe(2);
+    });
+
 });
--- product/warehouse/src/webapp/src/app/methods.component.ts
+++ product/warehouse/src/webapp/src/app/methods.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WarehouseService} from './warehouse.service';
 import {MethodData} from './model/MethodData';
 import {AgePipe} from './age.pipe';
@@ -11,26 +11,53 @@ import {Method} from './model/Method';
     styles: [require('./methods.component.css')],
     providers: [WarehouseService, AgePipe, DatePipe],
 })
-export class MethodsComponent {
-
+export class MethodsComponent implements OnInit {
     signature: string;
     maxResults = 100;
     data: MethodData;
     errorMessage: string;
     dateFormat = 'age';
-    sortColumn = 'signature';
+    sortColumn: string;
     sortAscending = true;
 
     constructor(private warehouse: WarehouseService) {
     }
 
-    sortBy(column: string) {
+    ngOnInit(): void {
+        this.sortBySignature();
+    }
+
+    private sortBy(column: string) {
         if (this.sortColumn === column) {
             this.sortAscending = !this.sortAscending;
         } else {
             this.sortColumn = column;
         }
         console.log(`Sorting by ${this.sortColumn}, ascending=${this.sortAscending}`);
+    }
+
+    sortBySignature() {
+        this.sortBy('signature');
+    }
+
+    sortByAge() {
+        this.sortBy('age');
+    }
+
+    sortedMethods(): Method[] {
+        if (!this.data || !this.data.methods) {
+            return null;
+        }
+
+        return this.data.methods.sort((m1: Method, m2: Method) => {
+            let cmp = 0;
+            if (this.sortColumn === 'signature') {
+                cmp = m1.signature.localeCompare(m2.signature);
+            } else if (this.sortColumn === 'age') {
+                cmp = m1.lastInvokedAtMillis - m2.lastInvokedAtMillis;
+            }
+            return this.sortAscending ? cmp : -cmp;
+        });
     }
 
     search() {
@@ -45,27 +72,4 @@ export class MethodsComponent {
             }, () => console.log('getMethods() complete'));
     }
 
-    sortedMethods(): Method[] {
-        if (!this.data) {
-            return null;
-        }
-        const greaterThan = this.sortAscending ? +1 : -1;
-        return this.data.methods.sort((m1, m2) => {
-           if (this.sortColumn === 'signature') {
-               if (m1.signature > m2.signature) {
-                   return greaterThan;
-               } else if (m1.signature === m2.signature) {
-                   return 0;
-               } else {
-                   return -greaterThan;
-               }
-           } else if (this.sortColumn === 'age') {
-               return this.sortAscending
-                   ? m2.lastInvokedAtMillis - m1.lastInvokedAtMillis
-                   : m1.lastInvokedAtMillis - m2.lastInvokedAtMillis;
-           } else {
-               return 0;
-           }
-        });
-    }
 }
