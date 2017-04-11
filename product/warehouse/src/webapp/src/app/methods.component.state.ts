d2026fb418eb0ca81dc8e87fade08347a2882dbf
ollehallin <olle.hallin@crisp.se>
2017-04-11 15:30:27 +0200

    Moved MethodsComponentState to own file.

---
+++ product/warehouse/src/webapp/src/app/methods.component.state.ts
@@ -0,0 +1,109 @@
+/**
+ * The state for MethodsComponent.
+ */
+import {MethodData} from './model/MethodData';
+import {MethodsComponent} from './methods.component';
+import {Method} from './model/Method';
+import {WarehouseService} from './warehouse.service';
+
+export class MethodsComponentState {
+    signature: string;
+    maxResults = 100;
+    data: MethodData;
+    errorMessage: string;
+    dateFormat = 'age';
+    sortColumn = MethodsComponent.SIGNATURE_COLUMN;
+    sortAscending = true;
+    selectedMethod: Method;
+
+    constructor(private warehouse: WarehouseService) {
+        console.log('Created MethodsComponentState')
+    }
+
+    private sortBy(column: string) {
+        if (this.sortColumn === column) {
+            this.sortAscending = !this.sortAscending;
+        } else {
+            this.sortColumn = column;
+        }
+        console.log(`Sorting by ${this.sortColumn}, ascending=${this.sortAscending}`);
+    }
+
+    private getHeaderIconClassesFor(column: string) {
+        return {
+            'fa': true,
+            'fa-sort-asc': this.sortAscending,
+            'fa-sort-desc': !this.sortAscending,
+            'invisible': column !== this.sortColumn // avoid column width fluctuations
+        };
+    }
+
+    headerIconClassesSignature() {
+        return this.getHeaderIconClassesFor(MethodsComponent.SIGNATURE_COLUMN);
+    }
+
+    headerIconClassesAge() {
+        return this.getHeaderIconClassesFor(MethodsComponent.AGE_COLUMN);
+    }
+
+    sortBySignature() {
+        this.sortBy(MethodsComponent.SIGNATURE_COLUMN);
+    }
+
+    sortByAge() {
+        this.sortBy(MethodsComponent.AGE_COLUMN);
+    }
+
+    sortedMethods(): Method[] {
+        if (!this.data || !this.data.methods) {
+            return null;
+        }
+
+        return this.data.methods.sort((m1: Method, m2: Method) => {
+            let cmp = 0;
+            if (this.sortColumn === MethodsComponent.SIGNATURE_COLUMN) {
+                cmp = m1.signature.localeCompare(m2.signature);
+            } else if (this.sortColumn === MethodsComponent.AGE_COLUMN) {
+                cmp = m1.lastInvokedAtMillis - m2.lastInvokedAtMillis;
+            }
+            if (cmp === 0) {
+                // Make sure the sorting is stable
+                cmp = m1.id - m2.id;
+            }
+            return this.sortAscending ? cmp : -cmp;
+        });
+    }
+
+    search() {
+        this.warehouse
+            .getMethods(this.signature, this.maxResults)
+            .subscribe(data => {
+                this.data = data;
+                this.errorMessage = undefined;
+                if (this.data.methods.length === 1) {
+                    this.selectMethod(this.data.methods[0]);
+                } else if (this.selectedMethod) {
+                    console.log('Trying to find %o', this.selectedMethod);
+                    let previouslySelected = this.data.methods.find(m => m.id === this.selectedMethod.id);
+                    console.log('Previously selected: %o', previouslySelected);
+                    this.selectMethod(previouslySelected);
+                } else {
+                    this.selectMethod(null);
+                }
+            }, error => {
+                this.data = undefined;
+                this.errorMessage = error.statusText ? error.statusText : error;
+                this.selectMethod(null);
+            }, () => console.log('getMethods() complete'));
+    }
+
+    selectMethod(m: Method) {
+        this.selectedMethod = m;
+    }
+
+    isSelectedMethod(m: Method) {
+        return this.selectedMethod && this.selectedMethod.id === m.id;
+    }
+
+}
+
--- product/warehouse/src/webapp/src/app/methods.component.ts
+++ product/warehouse/src/webapp/src/app/methods.component.ts
@@ -1,108 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {WarehouseService} from './warehouse.service';
-import {MethodData} from './model/MethodData';
 import {AgePipe} from './age.pipe';
 import {DatePipe} from '@angular/common';
-import {Method} from './model/Method';
 import {Router} from '@angular/router';
 import {StateService} from './state.service';
-
-class MethodsComponentState {
-    signature: string;
-    maxResults = 100;
-    data: MethodData;
-    errorMessage: string;
-    dateFormat = 'age';
-    sortColumn = MethodsComponent.SIGNATURE_COLUMN;
-    sortAscending = true;
-    selectedMethod: Method;
-
-    constructor(private warehouse: WarehouseService) {
-        console.log('Created MethodsComponentState')
-    }
-
-    private sortBy(column: string) {
-        if (this.sortColumn === column) {
-            this.sortAscending = !this.sortAscending;
-        } else {
-            this.sortColumn = column;
-        }
-        console.log(`Sorting by ${this.sortColumn}, ascending=${this.sortAscending}`);
-    }
-
-    private getHeaderIconClassesFor(column: string) {
-        return {
-            'fa': true,
-            'fa-sort-asc': this.sortAscending,
-            'fa-sort-desc': !this.sortAscending,
-            'invisible': column !== this.sortColumn // avoid column width fluctuations
-        };
-    }
-
-    headerIconClassesSignature() {
-        return this.getHeaderIconClassesFor(MethodsComponent.SIGNATURE_COLUMN);
-    }
-
-    headerIconClassesAge() {
-        return this.getHeaderIconClassesFor(MethodsComponent.AGE_COLUMN);
-    }
-
-    sortBySignature() {
-        this.sortBy(MethodsComponent.SIGNATURE_COLUMN);
-    }
-
-    sortByAge() {
-        this.sortBy(MethodsComponent.AGE_COLUMN);
-    }
-
-    sortedMethods(): Method[] {
-        if (!this.data || !this.data.methods) {
-            return null;
-        }
-
-        return this.data.methods.sort((m1: Method, m2: Method) => {
-            let cmp = 0;
-            if (this.sortColumn === MethodsComponent.SIGNATURE_COLUMN) {
-                cmp = m1.signature.localeCompare(m2.signature);
-            } else if (this.sortColumn === MethodsComponent.AGE_COLUMN) {
-                cmp = m1.lastInvokedAtMillis - m2.lastInvokedAtMillis;
-            }
-            if (cmp === 0) {
-                // Make sure the sorting is stable
-                cmp = m1.id - m2.id;
-            }
-            return this.sortAscending ? cmp : -cmp;
-        });
-    }
-
-    search() {
-        this.warehouse
-            .getMethods(this.signature, this.maxResults)
-            .subscribe(data => {
-                this.data = data;
-                this.errorMessage = undefined;
-                if (this.data.methods.length === 1) {
-                    this.selectMethod(this.data.methods[0]);
-                } else {
-                    // TODO: only unselect if selectedMethod is absent in new data
-                    this.selectMethod(null);
-                }
-            }, error => {
-                this.data = undefined;
-                this.errorMessage = error.statusText ? error.statusText : error;
-                this.selectMethod(null);
-            }, () => console.log('getMethods() complete'));
-    }
-
-    selectMethod(m: Method) {
-        this.selectedMethod = m;
-    }
-
-    isSelectedMethod(m: Method) {
-        return this.selectedMethod !== null && this.selectedMethod.id === m.id;
-    }
-
-}
+import {MethodsComponentState} from './methods.component.state';
 
 @Component({
     selector: 'ck-methods',
