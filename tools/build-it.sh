72722e21ae62381b38495a7e1c4ff2bd00363b9c
ollehallin <olle.hallin@crisp.se>
2017-04-12 14:51:44 +0200

    Fixed a permissions problem caused by running as root inside containers.

--- tools/build-it.sh
+++ tools/build-it.sh
@@ -11,6 +11,9 @@ ${GRADLEW} --stop
 echo "Cleaning Gradle build state..."
 rm -fr ./.gradle
 
+echo "Cleaning /tmp/codekvast..."
+rm -fr /tmp/codekvast
+
 echo "Cleaning workspace..."
 find product -name build -type d | grep -v node_modules | xargs rm -fr
 
