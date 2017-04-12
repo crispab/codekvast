d9fe98a3e17b80060ac567ac129981d2fa824583
ollehallin <olle.hallin@crisp.se>
2017-04-12 10:23:51 +0200

    Added Google Analytics tracking.

--- product/warehouse/src/webapp/config/webpack.common.js
+++ product/warehouse/src/webapp/config/webpack.common.js
@@ -60,6 +60,11 @@ module.exports = {
                 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css',
                 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
             ],
+            devServer: 'http://localhost:8088',
+            googleAnalytics: {
+                trackingId: 'UA-97240168-1',
+                pageViewOnLoad: true
+            },
             mobile: true,
             minify: false,
             appMountId: 'app',
