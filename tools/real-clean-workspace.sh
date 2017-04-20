8a96bf1d2147154f5cfbdb78f478d27dd31270dd
Olle Hallin <olle.hallin@crisp.se>
2017-04-20 22:30:06 +0200

    Gradle: moved build cache from $HOME/.gradle/caches to rootDir/.build-cache

--- tools/real-clean-workspace.sh
+++ tools/real-clean-workspace.sh
@@ -8,9 +8,11 @@ cd $(dirname $0)/..
 echo "Cleaning Gradle build state..."
 rm -fr ./.gradle
 
+echo "Cleaning Gradle build cache..."
+rm -fr ./.build-cache
+
 echo "Cleaning /tmp/codekvast..."
 rm -fr /tmp/codekvast
 
 echo "Cleaning workspace..."
 find product -name build -type d | grep -v node_modules | xargs rm -fr
-
