89bba82cd3289921b28776c5c22dce87a46453c9
ollehallin <olle.hallin@crisp.se>
2017-05-19 14:18:24 +0200

    Added support AWS Elastic Load Balancer (still without SSL)

--- provisioning/files/ubuntu-cloud-init.sh
+++ provisioning/files/ubuntu-cloud-init.sh
@@ -2,9 +2,7 @@
 #------------------------------------------------------------------
 # This script is sent as user_data to the ec2 module.
 # It will execute as root immediately after the first boot
-#
-# NOTE: It is designed to be as quick as possible.
-# Slow tasks like `apt update` and `apt upgrade` are done from a playbook.
 #------------------------------------------------------------------
 
+apt update
 apt install -y python-minimal python-apt ntp aptitude
