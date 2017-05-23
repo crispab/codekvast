7a3cb34609901792eba6749cd270da3969af9ce7
Olle Hallin <olle.hallin@crisp.se>
2017-05-23 17:15:53 +0200

    Added some convenience scripts

---
+++ provisioning/provision-aws-infrastructure.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#---------------------------------------------------------------------------------------------------
+# Provisions AWS resources for the stacks that are defined by vars/common.yml
+#---------------------------------------------------------------------------------------------------
+
+for f in ~/.boto ~/.ssh/codekvast-amazon.pem; do
+    if [ ! -f $f ]; then
+        echo "Missing required file: $f" 1>&2
+        exit 1
+    fi
+done
+
+cd $(dirname $0)
+ansible-playbook --private-key ~/.ssh/codekvast-amazon.pem infrastructure.yml $*
+
