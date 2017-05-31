70a4298cc9c02743428b90a23d488a2d3ddc3292
ollehallin <olle.hallin@crisp.se>
2017-05-31 22:03:53 +0200

    Invokes method imported from sample-lib.

--- sample/sample-spring-boot-repackaged-jar/src/main/java/sample/springbootrepackaged/ButtonOneService.java
+++ sample/sample-spring-boot-repackaged-jar/src/main/java/sample/springbootrepackaged/ButtonOneService.java
@@ -1,16 +1,15 @@
 package sample.springbootrepackaged;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import sample.lib.used.Bar1;
 
 @Service
 @Slf4j
 public class ButtonOneService {
 
     @SuppressWarnings("unused")
-    public void doSomething() {
-        log.info("Doing something 1");
+    void doSomething() {
+        log.info("Doing something 1: {}", new Bar1().declaredOnBar());
     }
 }
