76653d215835ef0057ae9e5c78776ff030b63294
ollehallin <olle.hallin@crisp.se>
2017-06-01 09:45:18 +0200

    Added task sample-spring-boot-repackaged-jar:launchBootRepackagedJar

--- sample/sample-spring-boot-repackaged-jar/src/main/java/sample/springbootrepackaged/SampleSpringBootApp.java
+++ sample/sample-spring-boot-repackaged-jar/src/main/java/sample/springbootrepackaged/SampleSpringBootApp.java
@@ -1,14 +1,26 @@
 package sample.springbootrepackaged;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@Slf4j
 public class SampleSpringBootApp {
 
 	public static void main(String[] args) {
+        checkIfLoadedBySpringLoader();
 		SpringApplication.run(SampleSpringBootApp.class, args);
 	}
+
+    private static void checkIfLoadedBySpringLoader() {
+        try {
+            Class.forName("org.springframework.boot.loader.JarLauncher");
+            log.info("Loaded by Spring Boot Loader");
+        } catch (ClassNotFoundException e) {
+            log.info("Not loaded by Spring Boot Loader");
+        }
+    }
 }
