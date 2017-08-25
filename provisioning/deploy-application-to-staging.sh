36e104104e10b164212155ed0c87cc09edf645ee
ollehallin <olle.hallin@crisp.se>
2017-08-25 17:40:43 +0200

    Renamed some provisioning scripts

---
+++ provisioning/deploy-application-to-staging.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#---------------------------------------------------------------------------------------------------
+# Deploys Codekvast to the staging environment
+#---------------------------------------------------------------------------------------------------
+
+for f in ~/.boto ~/.ssh/codekvast-amazon.pem; do
+    if [ ! -f $f ]; then
+        echo "Missing required file: $f" 1>&2
+        exit 1
+    fi
+done
+
+cd $(dirname $0)
+ansible-playbook --private-key ~/.ssh/codekvast-amazon.pem playbooks/servers.yml --tags application --limit tag_Env_staging $*
---
+++ provisioning/os-maintenance.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#---------------------------------------------------------------------------------------------------
+# Upgrades the servers' operating system in prod
+#---------------------------------------------------------------------------------------------------
+
+for f in ~/.boto ~/.ssh/codekvast-amazon.pem; do
+    if [ ! -f $f ]; then
+        echo "Missing required file: $f" 1>&2
+        exit 1
+    fi
+done
+
+cd $(dirname $0)
+ansible-playbook --private-key ~/.ssh/codekvast-amazon.pem playbooks/os-maintenance.yml $*
