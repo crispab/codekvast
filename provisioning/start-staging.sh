215553682d4ff25313c3d94d2678b9c94f429967
Olle Hallin <olle.hallin@crisp.se>
2017-11-05 11:04:24 +0100

    Fixed start/stop staging scripts

--- provisioning/start-staging.sh
+++ provisioning/start-staging.sh
@@ -10,9 +10,13 @@ for f in ~/.boto ~/.ssh/codekvast-amazon.pem; do
     fi
 done
 
-aws --profile codekvast ec2 describe-instances --filter "Name=tag:Env,Values=staging" \
+cd $(dirname $0)
+declare region=$(grep aws_region playbooks/vars/common.yml | cut -d: -f2)
+declare AWS_EC2="aws --profile codekvast --region ${region} ec2"
+
+${AWS_EC2} describe-instances --filter "Name=tag:Env,Values=staging" \
      | awk '/InstanceId/{print $2}' | tr -d '",' | while read instance; do
      echo "Starting instance ${instance}..."
-    aws --profile codekvast ec2 start-instances --instance-ids ${instance}
+    ${AWS_EC2} start-instances --instance-ids ${instance}
 done
 
