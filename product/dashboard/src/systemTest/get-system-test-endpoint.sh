208bd8a2905c23407cda9ced940934565b5c671d
Olle Hallin <olle.hallin@schibsted.se>
2017-11-23 11:33:13 +0100

    Made systemTest work on Mac OS X

--- product/dashboard/src/systemTest/get-system-test-endpoint.sh
+++ product/dashboard/src/systemTest/get-system-test-endpoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare port=$(jps -m | grep codekvast-dashboard- | head --lines=1 | egrep --only-matching "server.port=[0-9]+" | cut -d= -f2)
+declare port=$(jps -m | grep codekvast-dashboard- | head -n 1 | egrep --only-matching "server.port=[0-9]+" | cut -d= -f2)
 if [ -n "$port" ]; then
     echo "http://localhost:$port"
 fi
