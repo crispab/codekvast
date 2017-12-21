a81a3586a3a73a8c2433b3a50465a1a1b49093f6
Olle Hallin <olle.hallin@crisp.se>
2017-12-21 17:52:26 +0100

    Corrected bug in .build-common.sh

--- tools/.build-common.sh
+++ tools/.build-common.sh
@@ -7,8 +7,8 @@ cd $(dirname $0)/..
 
 declare CODEKVAST_VERSION=$(grep codekvastVersion gradle.properties | egrep --only-matching '[0-9.]+')
 declare GIT_HASH=$(git rev-parse --short HEAD)
+declare COMMITTED_VERSION="${CODEKVAST_VERSION}-${GIT_HASH}"
 declare NUM_DIRTY_FILES=$(git status --porcelain | wc -l)
-declare COMMITTED_VERSION="${COMMITTED_VERSION}"
 declare BUILD_STATE_FILE=.buildState
 declare BUILT_FROM_VERSION=$(cat ${BUILD_STATE_FILE} 2>/dev/null)
 declare POST_COMMIT_HOOK=.git/hooks/post-commit
