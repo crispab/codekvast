50a93e80ffc7df8c0cac375415b7dec400e384f5
Olle Hallin <olle.hallin@crisp.se>
2017-12-21 10:39:44 +0100

    Bug fixes in prepare-workstation

--- tools/prepare-workstation/run.sh
+++ tools/prepare-workstation/run.sh
@@ -7,15 +7,14 @@ if [ "$(which ansible)" == "/usr/local/bin/ansible" ]; then
     sudo pip uninstall ansible
 fi
 
-if [ "$(which ansible)" == "" -o  "$(which ohai)" == "" ]; then
-    grep -q "ppa.launchpad.net/ansible/ansible" /etc/apt/sources.list.d/ansible*.list || {
-        echo "Adding APT key for ppa:ansible/ansible:"
-        sudo apt install -y software-properties-common
-        sudo apt-add-repository -y ppa:ansible/ansible
-    }
+if [ "$(grep ppa.launchpad.net/ansible/ansible /etc/apt/sources.list.d/ansible*.list)" == "" -o  "$(which ohai)" == "" ]; then
+    echo "Adding APT key for ppa:ansible/ansible:"
+    sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt update
-    sudo apt install -y ansible ohai libssl-dev
+    sudo apt install -y software-properties-common ansible ohai libssl-dev
 fi
 
 cd $(dirname $0)/ansible
+sudo chown root.root ~/.netrc
 sudo ansible-playbook -i inventory playbook.yml -e "actual_username=${USER}" $*
+sudo chown ${USER}.$(id -gn ${USER}) ~/.netrc
