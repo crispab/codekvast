2448231cd785175284b84a407b18053afae2ffa9
Olle Hallin <olle.hallin@crisp.se>
2017-12-21 15:30:45 +0100

    Moved installing post-commit hook to real-clean-workspace.sh

--- tools/build-it.sh
+++ tools/build-it.sh
@@ -7,7 +7,7 @@ declare GRADLEW=./gradlew
 declare GRADLE_OPTS="${GRADLE_OPTS:--Dorg.gradle.configureondemand=false}"
 declare CODEKVAST_VERSION=$(grep codekvastVersion gradle.properties | egrep --only-matching '[0-9.]+')
 declare GIT_HASH=$(git rev-parse --short HEAD)
-declare BUILD_STATE_FILE=$(pwd)/.buildState
+declare BUILD_STATE_FILE=.buildState
 declare lastBuilt=$(cat ${BUILD_STATE_FILE} 2>/dev/null)
 if [ "$lastBuilt" == "${CODEKVAST_VERSION}-${GIT_HASH}" -a $(git status --porcelain | wc -l) -eq 0 ]; then
   echo "Build is up-to-date with ${CODEKVAST_VERSION}-${GIT_HASH} and workspace is clean; will not build"
@@ -38,17 +38,4 @@ ${GRADLEW} ${GRADLE_OPTS} coverageReport
 if [ $(git status --porcelain | wc -l) -eq 0 ]; then
     echo "Recording that $CODEKVAST_VERSION-$GIT_HASH has been build..."
     echo "$CODEKVAST_VERSION-$GIT_HASH" > ${BUILD_STATE_FILE}
-fi
-
-declare postCommitHook=.git/hooks/post-commit
-if [ ! -f "$postCommitHook" ]; then
-    echo "Installing Git post-commit hook..."
-    cat << EOF > ${postCommitHook}
-#!/bin/bash
-rm -f ${BUILD_STATE_FILE}
-EOF
-    chmod +x ${postCommitHook}
-elif [ $(grep ${BUILD_STATE_FILE} ${postCommitHook} | wc -l) == 0 ]; then
-    echo "Augmenting Git post-commit hook..."
-    echo "rm -f ${BUILD_STATE_FILE}" >> ${postCommitHook}
 fi
--- tools/real-clean-workspace.sh
+++ tools/real-clean-workspace.sh
@@ -8,10 +8,24 @@ cd $(dirname $0)/..
 declare CODEKVAST_VERSION=$(grep codekvastVersion gradle.properties | egrep --only-matching '[0-9.]+')
 declare GIT_HASH=$(git rev-parse --short HEAD)
 declare BUILD_STATE_FILE=.buildState
-declare lastBuilt=$(cat ${BUILD_STATE_FILE} 2>/dev/null)
-if [ "$lastBuilt" == "${CODEKVAST_VERSION}-${GIT_HASH}" -a $(git status --porcelain | wc -l) -eq 0 ]; then
+declare postCommitHook=.git/hooks/post-commit
+declare buildState=$(cat ${BUILD_STATE_FILE} 2>/dev/null)
+
+if [ "$buildState" == "${CODEKVAST_VERSION}-${GIT_HASH}" -a $(git status --porcelain | wc -l) -eq 0 ]; then
   echo "Build is up-to-date with ${CODEKVAST_VERSION}-${GIT_HASH} and workspace is clean; will not clean"
   exit 0
+fi
+
+if [ ! -f "$postCommitHook" ]; then
+    echo "Installing Git post-commit hook..."
+    cat << EOF > ${postCommitHook}
+#!/bin/bash
+rm -f ${BUILD_STATE_FILE}
+EOF
+    chmod +x ${postCommitHook}
+elif [ $(grep ${BUILD_STATE_FILE} ${postCommitHook} | wc -l) == 0 ]; then
+    echo "Augmenting Git post-commit hook..."
+    echo "rm -f ${BUILD_STATE_FILE}" >> ${postCommitHook}
 fi
 
 echo "Cleaning Gradle build state..."
