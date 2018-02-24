13037bbafb6d6162fde64946be8dcf98ff57f350
Olle Hallin <olle.hallin@crisp.se>
2018-02-24 23:42:47 +0100

    Added playbook for rebooting servers.

---
+++ provisioning/reboot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#---------------------------------------------------------------------------------------------------
+# Upgrades the servers' operating system in prod
+#---------------------------------------------------------------------------------------------------
+
+source $(dirname $0)/.check-requirements.sh
+
+ansible-playbook --private-key ~/.ssh/codekvast-amazon.pem playbooks/reboot.yml --limit tag_Env_staging $*
+ansible-playbook --private-key ~/.ssh/codekvast-amazon.pem playbooks/reboot.yml --limit tag_Env_prod $*
