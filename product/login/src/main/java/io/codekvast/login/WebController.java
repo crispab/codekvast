257346baa99321fd9e11f8bb7310c69a9c40a7ee
Olle Hallin <olle.hallin@crisp.se>
2018-02-25 00:12:36 +0100

    Renamed Ansible variable (environment was a reserved name)

--- product/login/src/main/java/io/codekvast/login/WebController.java
+++ product/login/src/main/java/io/codekvast/login/WebController.java
@@ -26,7 +26,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -51,7 +50,7 @@ public class WebController {
 
     @RequestMapping("/unauthenticated")
     public String unauthenticated() {
-        logger.info("Unauthenticated");
+        logger.info("/unauthenticated");
         return "redirect:/?error=true";
     }
 
