298fe377cae5bdccaf9221b00d09d3d4a7f5fa27
Olle Hallin <olle.hallin@crisp.se>
2018-04-16 23:00:37 +0200

    Fixed port conflict in CodekvastAdminApplicationTest

--- product/admin/src/test/java/io/codekvast/admin/CodekvastAdminApplicationTest.java
+++ product/admin/src/test/java/io/codekvast/admin/CodekvastAdminApplicationTest.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = CodekvastAdminApplication.class)
-public class CodekvastAdminApplicationTests {
+@SpringBootTest(
+    classes = CodekvastAdminApplication.class,
+    properties = {"server.port=0", "management.server.port=0"})
+public class CodekvastAdminApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
