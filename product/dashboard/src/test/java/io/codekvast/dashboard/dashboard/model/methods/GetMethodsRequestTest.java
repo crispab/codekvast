e289a816f1b4202aafc1d7a7f5c6e455fc4cdfe9
Olle Hallin <olle.hallin@crisp.se>
2018-04-29 23:21:43 +0200

    Fixed broken test

--- product/dashboard/src/test/java/io/codekvast/dashboard/dashboard/model/methods/GetMethodsRequestTest.java
+++ product/dashboard/src/test/java/io/codekvast/dashboard/dashboard/model/methods/GetMethodsRequestTest.java
@@ -19,18 +19,18 @@ public class GetMethodsRequestTest {
     @Test
     public void should_normalize_signature_containing_no_wildcards() {
         GetMethodsRequest req = GetMethodsRequest.defaults().toBuilder().signature("foobar").build();
-        assertThat(req.getNormalizedSignature(), is("%foobar%"));
+        assertThat(req.getNormalizedSignature(), is("foobar%"));
     }
 
     @Test
     public void should_normalize_signature_containing_asterisk() {
         GetMethodsRequest req = GetMethodsRequest.defaults().toBuilder().signature("foo*bar").build();
-        assertThat(req.getNormalizedSignature(), is("%foo%bar%"));
+        assertThat(req.getNormalizedSignature(), is("foo%bar%"));
     }
 
     @Test
     public void should_normalize_signature_containing_question_mark() {
         GetMethodsRequest req = GetMethodsRequest.defaults().toBuilder().signature("foo?bar").build();
-        assertThat(req.getNormalizedSignature(), is("%foo_bar%"));
+        assertThat(req.getNormalizedSignature(), is("foo_bar%"));
     }
 }
