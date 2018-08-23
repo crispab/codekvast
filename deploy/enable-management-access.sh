2ba9c1f685bf7dbe7a1ab1218723048dd691d929
Olle Hallin <olle.hallin@klarna.com>
2018-08-23 11:47:06 +0200

    WIP: enable-management-access.sh

---
+++ deploy/enable-management-access.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#---------------------------------------------------------------------------------------------------------
+# Modifies an EC2 security group to enable access to certain ports from this workstation
+#---------------------------------------------------------------------------------------------------------
+
+declare environment=${1:-staging}
+declare description=${2:-$USER}
+
+declare groupName=codekvast-default-${environment}-management
+declare myIp=$(curl -s https://api.ipify.org)
+
+# Find the GroupId of the security group
+declare groupId=$(aws --profile codekvast ec2  describe-security-groups --filters Name=group-name,Values=${groupName}|jq .SecurityGroups[0].GroupId|xargs)
+
+case "$groupId" in
+    null)
+        echo "No such security group: $groupName"
+        exit 1;;
+    sg-*)
+    ;;
+esac
+
+echo -n "Will modify the security group $groupName to enable TCP access from $myIp to ports 22,3306,8080-8082 with the description \"$description\". Ok? [y/N]: "
+read answer
+case $answer in
+    ""|n|N) exit 0;;
+    y|Y) ;;
+esac
+
+echo "Ok, going ahead..."
+# aws --profile codekvast ec2 authorize-security-group ...
+
+
