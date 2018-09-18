d8fcfc9e3072cf70260db4db6110b69c8c81cae5
Olle Hallin <olle.hallin@crisp.se>
2018-09-18 17:12:56 +0200

    Replaced playboot for rebooting instances with AWS CLI script. Upgraded to Gradle 4.10.1

--- deploy/reboot-staging.sh
+++ deploy/reboot-staging.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 #---------------------------------------------------------------------------------------------------
-# Upgrades the servers' operating system in prod
+# Reboots all prod instances using the AWS CLI
 #---------------------------------------------------------------------------------------------------
 
 source $(dirname $0)/.check-requirements.sh
 
-ansible-playbook --private-key ~/.ssh/codekvast-amazon.pem playbooks/reboot.yml --limit tag_Env_staging $*
+echo -n "Are you sure [N/y]: "; read answer
+case "$answer" in
+    ""|"N"|"n") exit 0;;
+    "y"|"Y") echo "Ok, here we go...";;
+esac
+
+declare region=$(grep aws_region playbooks/vars/common.yml | cut -d: -f2)
+declare AWS_EC2="aws --profile codekvast --region ${region} ec2"
+
+${AWS_EC2} describe-instances --filter "Name=tag:Env,Values=staging" \
+     | awk '/InstanceId/{print $2}' | tr -d '",' | while read instance; do
+    echo "Rebooting instance ${instance}..."
+    ${AWS_EC2} reboot-instances --instance-ids ${instance}
+done
+
--- deploy/reboot.sh
+++ deploy/reboot.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 #---------------------------------------------------------------------------------------------------
-# Upgrades all servers' operating system
+# Reboots one instance using the AWS CLI
 #---------------------------------------------------------------------------------------------------
 
 source $(dirname $0)/.check-requirements.sh
 
-ansible-playbook --private-key ~/.ssh/codekvast-amazon.pem playbooks/reboot.yml $*
+if [ -z "$1" ]; then
+    echo "Usage: $(basename $0) instance-id-or-CNAME" >&2
+    exit 1
+fi
+
+declare region=$(grep aws_region playbooks/vars/common.yml | cut -d: -f2)
+declare AWS_EC2="aws --profile codekvast --region ${region} ec2"
+
+case "$1" in
+    i-*) instance=$1 ;;
+    *) instance=$(${AWS_EC2} describe-instances --filter Name=tag:CNAME,Values=$1 | awk '/InstanceId/{print $2}' | tr -d '",');;
+esac
+
+echo -n "About to reboot ${instance}. Are you sure [N/y]: "; read answer
+case "$answer" in
+    ""|"N"|"n") exit 0;;
+    "y"|"Y") echo "Ok, here we go...";;
+esac
+
+echo "Rebooting instance ${instance}..."
+${AWS_EC2} reboot-instances --instance-ids ${instance}
