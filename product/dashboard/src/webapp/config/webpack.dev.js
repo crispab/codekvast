768646b76d53a93c8ad91fe09709b1dbc6b15069
Olle Hallin <olle.hallin@crisp.se>
2018-10-23 23:07:49 +0200

    Corrected Webpack chunk name in dev.

--- product/dashboard/src/webapp/config/webpack.dev.js
+++ product/dashboard/src/webapp/config/webpack.dev.js
@@ -14,7 +14,7 @@ module.exports = webpackMerge(commonConfig, {
         path: helpers.root('dist'),
         publicPath: '/',
         filename: '[name].js',
-        chunkFilename: '[id].chunk.js'
+        chunkFilename: '[name].chunk.js'
     },
 
     devServer: {
