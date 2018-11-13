7afda824b6637b2a3dd7983cd3abacc43e0c814a
Olle Hallin <olle.hallin@crisp.se>
2018-11-13 23:04:55 +0100

    Made system-test more generic, it now also starts codekvast-login before test.

--- product/system-test/src/test/bin/stop-system-test-environment.sh
+++ product/system-test/src/test/bin/stop-system-test-environment.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-pkill -f codekvast-dashboard-
-
+pkill -f codekvast-dashboard- && echo "codekvast-dashboard killed"
+pkill -f codekvast-login- && echo "codekvast-login killed"
 docker ps | grep codekvast_systest_ | cut -d' ' -f1 | xargs --no-run-if-empty docker stop
-
 docker ps -a | grep codekvast_systest_ | cut -d' ' -f1 | xargs --no-run-if-empty docker rm
