3f6e2d85498f1b2f248b8ee28d20901fc88a5dc7
Olle Hallin <olle.hallin@crisp.se>
2018-11-21 22:05:34 +0100

    Upgraded mockito.

--- tools/uptodate-report.sh
+++ tools/uptodate-report.sh
@@ -7,6 +7,7 @@ mkdir -p $(dirname ${REPORT_FILE})
 
 echo "Java:" | tee ${REPORT_FILE}
 cd $(dirname $0)/../product
+# Exclude dependencies with latest versions that does not work in Java7
 ../gradlew uptodate --max-workers=1 | egrep "^'" | sort -u | tr -d "'" | egrep -v "com.google.guava:guava:|javax.validation:validation-api:" | tee -a ${REPORT_FILE}
 
 echo -e "\nDashboard JavaScript:" | tee -a ${REPORT_FILE}
