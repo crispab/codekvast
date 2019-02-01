c15af8823840342cfb6d028e7c7c526a75eb19f3
Olle Hallin <olle.hallin@crisp.se>
2019-02-01 10:40:23 +0100

    Setting OpenJDK 11 as default again (since OpenJDK 10 is end-of-life).

--- tools/install-compilers.sh
+++ tools/install-compilers.sh
@@ -17,15 +17,20 @@ set -e
 cd $(dirname $0)
 awk '/sdkmanJavaVersion_/ {print $3}' ../gradle.properties |while read version; do
     if test -x $HOME/.sdkman/candidates/java/${version}/bin/javac; then
-        echo JavaSDK ${version} already installed
+        echo Java SDK ${version} is already installed
     else
         echo sdk install java ${version}
         sdk install java ${version}
     fi
 done
 
-sdk default java $(grep sdkmanJavaDefault ../gradle.properties | awk '{print $3}')
-sdk list java
+declare defaultJava=$(grep sdkmanJavaDefault ../gradle.properties | awk '{print $3}')
+case $(readlink $HOME/.sdkman/candidates/java/current) in
+    $HOME/.sdkman/candidates/java/${defaultJava})
+        echo Java SDK ${defaultJava} is already default;;
+    *)
+        sdk default java ${defaultJava};;
+esac
 
 if test -f $HOME/.sdkman/etc/config${configBackupSuffix}; then
     mv -f $HOME/.sdkman/etc/config${configBackupSuffix} $HOME/.sdkman/etc/config
