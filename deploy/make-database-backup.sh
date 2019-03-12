e34e48f390662aa780d9e50816610eaa152b6e36
Olle Hallin <olle.hallin@crisp.se>
2019-03-12 08:46:57 +0100

    Refactored make-database-backup.*

--- deploy/make-database-backup.sh
+++ deploy/make-database-backup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #---------------------------------------------------------------------------------------------------
-# Dumps the production database to S3 using the backup name "extra"
+# Dumps the production database to S3 using yesterday as backup name
 #---------------------------------------------------------------------------------------------------
 
 source $(dirname $0)/.check-requirements.sh
-declare weekday=${1:-extra}
+
+declare weekday=${1:-$(env LANG=en_US date -d "yesterday 13:00" --utc +%A | tr [A-Z] [a-z])}
 
 ansible-playbook --private-key ~/.ssh/codekvast-amazon.pem playbooks/make-database-backup.yml -e weekday=${weekday}
