b5a607136dbd30457385ba468cfa39e954094496
Olle Hallin <olle.hallin@crisp.se>
2019-04-07 23:25:50 +0200

    Copied over is-logged-in.guard and state.service from old webapp

---
+++ product/server/dashboard/src/webapp2/src/app/guards/is-logged-in.guard.ts
@@ -0,0 +1,23 @@
+import {CanActivate, Router} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {CookieService} from 'ngx-cookie';
+import {StateService} from '../services/state.service';
+
+@Injectable()
+export class IsLoggedIn implements CanActivate {
+
+    constructor(private cookieService: CookieService, private stateService: StateService, private router: Router) {
+    }
+
+    canActivate() {
+        let sessionToken = this.cookieService.get('sessionToken');
+
+        if (sessionToken === null || sessionToken === undefined) {
+            this.stateService.setLoggedOut();
+            // noinspection JSIgnoredPromiseFromCall
+            this.router.navigateByUrl('not-logged-in');
+        }
+        return sessionToken != null;
+    }
+
+}
---
+++ product/server/dashboard/src/webapp2/src/app/services/state.service.ts
@@ -0,0 +1,66 @@
+/**
+ * Injectable holder for persistent state, such as form content, search results etc.
+ */
+import {CookieService} from 'ngx-cookie';
+import {Injectable} from '@angular/core';
+import {isNullOrUndefined} from 'util';
+import {Observable, Subject} from 'rxjs';
+
+export class AuthData {
+    readonly customerName: string;
+    readonly email: string;
+    readonly source: string;
+    readonly sourceApp: string;
+
+    constructor(customerName: string, email: string, source: string, sourceApp: string) {
+        this.customerName = customerName;
+        this.email = email;
+        this.source = source;
+        this.sourceApp = sourceApp;
+    }
+}
+
+@Injectable()
+export class StateService {
+
+    private state = {};
+    private authData = new Subject<AuthData>();
+    private loggedIn = false;
+
+    constructor(private cookieService: CookieService) {
+    }
+
+    getState<T>(key: string, initialState: () => T): T {
+        if (isNullOrUndefined(this.state[key])) {
+            this.state[key] = initialState();
+        }
+        return this.state[key];
+    }
+
+    getAuthData(): Observable<AuthData> {
+        return this.authData;
+    }
+
+    setLoggedInAs(customerName: string, email: string, source: string, sourceApp: string) {
+        let authData = new AuthData(customerName, email, source, sourceApp);
+        this.loggedIn = true;
+        this.authData.next(authData);
+    }
+
+    setLoggedOut() {
+        this.loggedIn = false;
+        this.authData.next(null);
+        this.cookieService.remove('sessionToken');
+        this.cookieService.remove('navData');
+    }
+
+    isLoggedIn() {
+        if (isNullOrUndefined(this.cookieService.get('sessionToken'))) {
+            if (this.loggedIn) {
+                console.log('[ck dashboard] Detected that sessionToken cookie has disappeared')
+            }
+            this.setLoggedOut();
+        }
+        return this.loggedIn;
+    }
+}
