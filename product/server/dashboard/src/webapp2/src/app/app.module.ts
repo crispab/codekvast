9625871c2b22d932b787798ac0731dfaaf1857cc
Olle Hallin <olle.hallin@crisp.se>
2019-04-07 22:57:20 +0200

    Copied over age.pipe from old webapp.

--- product/server/dashboard/src/webapp2/src/app/app.module.ts
+++ product/server/dashboard/src/webapp2/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgePipe } from './pipes/age.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgePipe
   ],
   imports: [
     BrowserModule,
