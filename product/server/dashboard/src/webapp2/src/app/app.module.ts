72fa5055e13452df5c92f633ca79e68af22d51d2
Olle Hallin <olle.hallin@crisp.se>
2019-04-07 23:07:24 +0200

    Copied over pipes from old webapp.

--- product/server/dashboard/src/webapp2/src/app/app.module.ts
+++ product/server/dashboard/src/webapp2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgePipe } from './pipes/age.pipe';
+import { InvocationStatusPipe } from './pipes/invocation-status.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AgePipe
+    AgePipe,
+    InvocationStatusPipe
   ],
   imports: [
     BrowserModule,
---
+++ product/server/dashboard/src/webapp2/src/app/pipes/invocation-status.pipe.ts
@@ -0,0 +1,21 @@
+import {Injectable, Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'ckInvocationStatus'}) @Injectable()
+export class InvocationStatusPipe implements PipeTransform {
+
+    transform(value: any): string {
+        if (typeof value === 'string') {
+            return this.prettyPrint(value);
+        }
+        if (Array.isArray(value)) {
+            return value.sort().map((v: any) => {
+                return typeof v === 'string' ? this.prettyPrint(v) : v == null ? null : v.toString();
+            }).join(', ');
+        }
+        return value == null ? null : value.toString();
+    }
+
+    private prettyPrint(v: string) {
+        return v.substr(0, 1).toUpperCase() + v.substr(1).toLowerCase().replace(/_/g, ' ');
+    }
+}
