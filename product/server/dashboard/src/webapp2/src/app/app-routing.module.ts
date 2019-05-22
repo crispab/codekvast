fdd822e7fc39f6fa095cc813385eafe662020d16
Olle Hallin <olle.hallin@crisp.se>
2019-05-23 00:24:14 +0200

    Copied over home.component, app.component, app.module and app-routing.module

--- product/server/dashboard/src/webapp2/src/app/app-routing.module.ts
+++ product/server/dashboard/src/webapp2/src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {CollectionStatusComponent} from './pages/collection-status/collection-status.component';
+import {HomeComponent} from './pages/home/home.component';
+import {IsLoggedIn} from './guards/is-logged-in.guard';
+import {MethodDetailsComponent} from './pages/methods/method-details/method-details.component';
+import {MethodsComponent} from './pages/methods/methods.component';
+import {NgModule} from '@angular/core';
+import {NotLoggedInComponent} from './pages/auth/not-logged-in/not-logged-in.component';
+import {ReportGeneratorComponent} from './pages/report-generator/report-generator.component';
+import {RouterModule, Routes} from '@angular/router';
+import {VoteResultComponent} from './pages/vote-result/vote-result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }, {
+        path: 'home',
+        component: HomeComponent
+    }, {
+        path: 'methods',
+        component: MethodsComponent,
+        canActivate: [IsLoggedIn]
+    }, {
+        path: 'method/:id',
+        component: MethodDetailsComponent,
+        canActivate: [IsLoggedIn]
+    }, {
+        path: 'not-logged-in',
+        component: NotLoggedInComponent
+    }, {
+        path: 'status',
+        component: CollectionStatusComponent,
+        canActivate: [IsLoggedIn]
+    }, {
+        path: 'reports',
+        component: ReportGeneratorComponent
+    }, {
+        path: 'vote-result/:feature/:vote',
+        component: VoteResultComponent
+    }, {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
