3bc2a8e6ee327490584ff598c75f2325b5736ea7
Olle Hallin <olle.hallin@crisp.se>
2019-05-22 21:14:46 +0200

    Fixed vote-result.component.spec.ts

--- product/server/dashboard/src/webapp2/src/app/pages/vote-result/vote-result.component.spec.ts
+++ product/server/dashboard/src/webapp2/src/app/pages/vote-result/vote-result.component.spec.ts
@@ -1,14 +1,25 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {VoteResultComponent} from './vote-result.component';
+import {Location} from '@angular/common';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Observable} from 'rxjs';
 
 describe('VoteResultComponent', () => {
     let component: VoteResultComponent;
     let fixture: ComponentFixture<VoteResultComponent>;
 
+    let locationStub: Partial<Location> = {back: function () {}};
+    let activatedRouteStub: Partial<ActivatedRoute> = {params: new Observable<Params>()};
+
     beforeEach(async(() => {
+        // noinspection JSIgnoredPromiseFromCall
         TestBed.configureTestingModule({
-                   declarations: [VoteResultComponent]
+                   declarations: [VoteResultComponent],
+                   providers: [
+                       {provide: Location, useValue: locationStub},
+                       {provide: ActivatedRoute, useValue: activatedRouteStub}
+                   ]
                })
                .compileComponents();
     }));
@@ -19,7 +30,7 @@ describe('VoteResultComponent', () => {
         fixture.detectChanges();
     });
 
-    xit('should create', () => {
+    it('should create', () => {
         expect(component).toBeTruthy();
     });
 });
