59af561598295d6cbd98e3c0f44288586566da1b
Olle Hallin <olle.hallin@crisp.se>
2019-10-15 18:00:35 +0200

    Enabled ChromeHeadless in Angular tests.

--- product/server/dashboard/src/webapp2/src/app/app.component.spec.ts
+++ product/server/dashboard/src/webapp2/src/app/app.component.spec.ts
@@ -1,16 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule, NgbModule
+            ],
+            declarations: [
+                AppComponent
+            ],
     }).compileComponents();
   }));
 
