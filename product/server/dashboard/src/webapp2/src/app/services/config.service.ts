5c03c396cc8bd6383032a08818fefc28fb1eb7d0
Olle Hallin <olle.hallin@crisp.se>
2019-10-16 08:23:11 +0200

    WIP: Inject CODEKVAST_DISPLAY_VERSION into webapp

--- product/server/dashboard/src/webapp2/src/app/services/config.service.ts
+++ product/server/dashboard/src/webapp2/src/app/services/config.service.ts
@@ -3,14 +3,21 @@
  */
 import {Injectable} from '@angular/core';
 import {ServerSettings} from '../model/server-settings';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class ConfigService {
 
     private serverSettings = new ServerSettings();
+    // noinspection TypeScriptFieldCanBeMadeReadonly
+    private version = 'dev';
+
+    constructor() {
+        this.version = environment.codekvastVersion;
+    }
 
     getVersion(): String {
-        return window['CODEKVAST_VERSION'] || 'dev';
+        return this.version;
     }
 
     getServerSettings() {
