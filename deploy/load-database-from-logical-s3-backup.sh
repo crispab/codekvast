9e623ec76cd71074bbefebd7e2479a34cba40852
Olle Hallin <olle.hallin@crisp.se>
2019-10-23 23:03:01 +0200

    Removed the ability to load a physical backup into a local MariaDB.

--- deploy/load-database-from-logical-s3-backup.sh
+++ deploy/load-database-from-logical-s3-backup.sh
@@ -7,9 +7,7 @@ source $(dirname $0)/.check-requirements.sh
 
 declare weekday=${1:-$(env LANG=en_US date -d "yesterday 13:00" --utc +%A | tr [A-Z] [a-z])}
 declare srcEnv=${2:-prod}
-declare dumpfile=mysqldump-${weekday}.sql.gz
-
-declare targetHost=localhost #codekvast-default-staging.cahjor9xtqud.eu-central-1.rds.amazonaws.com
+declare targetHost=${3:-localhost} #codekvast-default-staging.cahjor9xtqud.eu-central-1.rds.amazonaws.com
 
 case ${weekday} in
   monday|tuesday|wednesday|thursday|friday|saturday|sunday|extra);;
@@ -28,7 +26,7 @@ if [[ -z $(which mysql) ]]; then
   exit 1
 fi
 
-echo -n "About to load the ${weekday} mysqldump from ${srcEnv} into ${targetHost}:3306. Continue [y/N]: "
+echo -n "About to load the ${weekday} mysqldump from ${srcEnv} into jdbc:mariadb://${targetHost}:3306/codekvast. Continue [y/N]: "
 read answer
 case ${answer} in
     y|yes)
@@ -41,6 +39,7 @@ case ${answer} in
 esac
 
 declare s3_bucket="s3://io.codekvast.default.${srcEnv}.backup"
+declare dumpfile=mysqldump-${weekday}.sql.gz
 
 declare tmp_dir=$(mktemp -d /tmp/fetch-database.XXXXXXX)
 trap "rm -fr ${tmp_dir}" EXIT
@@ -48,5 +47,5 @@ trap "rm -fr ${tmp_dir}" EXIT
 echo "Fetching ${s3_bucket}/${dumpfile} ..."
 s3cmd get ${s3_bucket}/${dumpfile} ${tmp_dir}/${dumpfile}
 
-echo "Loading ${srcEnv} ${dumpfile} into ${targetHost}/codekvast ..."
-zcat ${tmp_dir}/${dumpfile} | mysql --protocol=tcp --user=codekvast --password --host=${targetHost} --database=codekvast
+echo "Loading ${srcEnv} ${dumpfile} into jdbc:mariadb://${targetHost}:3306/codekvast using the mysql client ..."
+zcat ${tmp_dir}/${dumpfile} | mysql --protocol=tcp --user=codekvast --password=codekvast --host=${targetHost} --database=codekvast
