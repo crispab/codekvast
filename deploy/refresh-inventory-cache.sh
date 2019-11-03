78f526ea6043e8324a33ebed74f8c0afdc4896a9
Olle Hallin <olle.hallin@crisp.se>
2019-11-03 20:51:57 +0100

    Bumped prod backends to t3a.small

--- deploy/refresh-inventory-cache.sh
+++ deploy/refresh-inventory-cache.sh
@@ -3,7 +3,7 @@
 # Refreshes the Ansible inventory cache
 #---------------------------------------------------------------------------------------------------
 
-rm -fr ~/.cache/ansible-inventory/
+rm -fr ~/.cache/ansible-inventory/ ~/.cache/ansible-facts/
 
 cd $(dirname $0)
 ansible-inventory --graph
