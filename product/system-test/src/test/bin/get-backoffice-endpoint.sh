a75b5d0e11b46906b0a8473a34fe11e90fa0b0e1
Olle Hallin <olle.hallin@klarna.com>
2019-11-04 14:21:57 +0100

    Extended system-test to also start RabbitMQ and codekvast-backoffice.

---
+++ product/system-test/src/test/bin/get-backoffice-endpoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+declare port=$(jps -m | grep "codekvast-backoffice-" | head -n 1 | egrep --only-matching "\-\-server.port=[0-9]+" | cut -d= -f2)
+if [[ -n "$port" ]]; then
+    echo "http://localhost:$port"
+fi
--- product/system-test/src/test/bin/stop-system-test-environment.sh
+++ product/system-test/src/test/bin/stop-system-test-environment.sh
@@ -2,5 +2,10 @@
 
 pkill -f codekvast-dashboard- && echo "codekvast-dashboard killed"
 pkill -f codekvast-login- && echo "codekvast-login killed"
-docker ps | grep codekvast_systest_db_ | cut -d' ' -f1 | xargs --no-run-if-empty docker stop
-docker ps -a | grep codekvast_systest_db_ | cut -d' ' -f1 | xargs --no-run-if-empty docker rm
+pkill -f codekvast-backoffice- && echo "codekvast-backoffice killed"
+
+docker ps | grep codekvast_systest_mariadb_ | cut -d' ' -f1 | xargs --no-run-if-empty docker stop
+docker ps -a | grep codekvast_systest_mariadb_ | cut -d' ' -f1 | xargs --no-run-if-empty docker rm
+
+docker ps | grep codekvast_systest_rabbitmq_ | cut -d' ' -f1 | xargs --no-run-if-empty docker stop
+docker ps -a | grep codekvast_systest_rabbitmq_ | cut -d' ' -f1 | xargs --no-run-if-empty docker rm
