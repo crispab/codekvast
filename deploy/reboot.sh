82bb27311ccec22e20d4e41cefd77271e9b71661
Olle Hallin <olle.hallin@crisp.se>
2019-11-14 20:16:58 +0100

    Now reboot.sh invokes /management/lbhealth to take the service out of the load balancer before rebooting.

--- deploy/reboot.sh
+++ deploy/reboot.sh
@@ -13,16 +13,34 @@ fi
 declare region=$(grep aws_region playbooks/vars/common.yml | cut -d: -f2)
 declare AWS_EC2="aws --profile codekvast --region ${region} ec2"
 
+declare haveCNAME=
+
 case "$1" in
     i-*) instance=$1 ;;
-    *) instance=$(${AWS_EC2} describe-instances --filter Name=tag:CNAME,Values="$1" | awk '/InstanceId/{print $2}' | tr -d '",');;
+    *) instance=$(${AWS_EC2} describe-instances --filter Name=tag:CNAME,Values="$1" | awk '/InstanceId/{print $2}' | tr -d '",')
+    haveCNAME=true;;
 esac
+
+if [[ -z "${instance}" ]]; then
+   echo "No such instance: $1" >&2
+   exit 1
+fi
 
 echo -n "About to reboot ${instance}. Are you sure [N/y]: "; read answer
 case "$answer" in
     ""|"N"|"n") exit 0;;
     "y"|"Y") echo "Ok, here we go...";;
 esac
+
+if [[ -n "${haveCNAME}" ]]; then
+    echo "Setting the load balancer health to Out of service..."
+    echo curl -X POST http://${1}:9081/management/lbhealth
+    curl -X POST http://${1}:9081/management/lbhealth
+
+    echo
+    echo "Sleeping 20 seconds..."
+    sleep 20
+fi
 
 echo "Rebooting instance ${instance}..."
 ${AWS_EC2} reboot-instances --instance-ids "${instance}"
