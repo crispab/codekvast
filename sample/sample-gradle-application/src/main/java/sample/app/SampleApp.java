e415acf072529e48b529a5af382a4c9c14192503
Olle Hallin <olle.hallin@crisp.se>
2019-11-27 09:04:26 +0100

    Improved SampleApp.

--- sample/sample-gradle-application/src/main/java/sample/app/SampleApp.java
+++ sample/sample-gradle-application/src/main/java/sample/app/SampleApp.java
@@ -28,8 +28,8 @@ public class SampleApp {
     @SneakyThrows(InterruptedException.class)
     private void run() {
         System.out.printf("Hello, World! from %s%n%n", getClass().getName());
-        tryToLoadClass("io.codekvast.javaagent.CodekvastAgent");
-        tryToLoadClass("org.aspectj.weaver.loadtime.Agent");
+        tryToLoadClass("io.codekvast.javaagent.CodekvastAgent", true);
+        tryToLoadClass("org.aspectj.weaver.loadtime.Agent", false);
 
         measureMethodCallTrackingOverhead();
 
@@ -55,18 +55,28 @@ public class SampleApp {
         invokeUntracked(count);
         invokeUntrackedLogged(count);
 
-        long untrackedElapsedMillis = invokeUntracked(count);
+        long untrackedNotLoggedElapsedMillis = invokeUntracked(count);
         long untrackedLoggedElapsedMillis = invokeUntrackedLogged(count);
         long trackedElapsedMillis = invokeTracked(count);
-        int overheadNanos = (int) ((trackedElapsedMillis - untrackedElapsedMillis) * 1000000d / (double) count);
 
-        System.out.printf("Invoked a trivial untracked        method %,d times in %5d ms%n", count, untrackedElapsedMillis);
-        System.out.printf("Invoked a trivial untracked logged method %,d times in %5d ms%n", count, untrackedLoggedElapsedMillis);
-        System.out.printf("Invoked a trivial   tracked        method %,d times in %5d ms%n", count, trackedElapsedMillis);
-        System.out.printf("Codekvast adds roughly %d ns to a method call%n", overheadNanos);
+        int overheadNanosUntrackedLoggedComparedToUntrackedUnlogged = (int) ((untrackedLoggedElapsedMillis - untrackedNotLoggedElapsedMillis) * 1000000d / (double) count);
+        int overheadNanosTrackedComparedToUntracked = (int) ((trackedElapsedMillis - untrackedNotLoggedElapsedMillis) * 1000000d / (double) count);
+        int overheadNanosTrackedComparedToLoggerCall =
+            (int) ((trackedElapsedMillis - untrackedLoggedElapsedMillis - untrackedNotLoggedElapsedMillis) * 1000000d / (double) count);
 
-        if (overheadNanos <= 5) {
-            throw new IllegalStateException("Unreasonable overhead: " + overheadNanos + ". Is Codekvast Agent correctly wired?");
+        System.out.println();
+        System.out.printf("Invoked a trivial untracked          method %,d times in %5d ms%n", count, untrackedNotLoggedElapsedMillis);
+        System.out.printf("Invoked a trivial untracked   logged method %,d times in %5d ms%n", count, untrackedLoggedElapsedMillis);
+        System.out.printf("Invoked a trivial   tracked unlogged method %,d times in %5d ms%n", count, trackedElapsedMillis);
+        System.out.println();
+        System.out.printf("Calling a disabled logger adds roughly %d ns compared to a plain method call%n", overheadNanosUntrackedLoggedComparedToUntrackedUnlogged);
+        System.out.printf("Codekvast                 adds roughly %d ns compared to a plain method call%n", overheadNanosTrackedComparedToUntracked);
+        System.out.printf("Codekvast                 adds roughly %d ns compared to a logger call%n", overheadNanosTrackedComparedToLoggerCall);
+        System.out.println();
+
+        if (overheadNanosTrackedComparedToUntracked <= 5) {
+            throw new IllegalStateException(
+                "Unreasonable overhead: " + overheadNanosTrackedComparedToUntracked + ". Is Codekvast Agent correctly wired?");
         }
     }
 
@@ -104,16 +114,15 @@ public class SampleApp {
         return System.currentTimeMillis() - startedAt;
     }
 
-    private void tryToLoadClass(String className) {
+    private void tryToLoadClass(String className, boolean expectedSuccess) {
         try {
             Class.forName(className);
 
-            String result = "is unavoidable.";
-            System.out.printf("GOOD:, %s can load class %s, which %s%n", SampleApp.class.getName(), className, result);
+            String result = expectedSuccess ? "GOOD" : "BAD";
+            System.out.printf("%s: %s can load class %s%n", result, SampleApp.class.getName(), className);
         } catch (ClassNotFoundException e) {
-
-            String result = ", which indicates that codekvast-agent is not enabled!";
-            System.out.printf("BAD:  %s cannot load class %s%s%n", SampleApp.class.getName(), className, result);
+            String result = expectedSuccess ? "BAD" : "GOOD";
+            System.out.printf("%s: %s cannot load class %s%n", result, SampleApp.class.getName(), className);
         }
     }
 
