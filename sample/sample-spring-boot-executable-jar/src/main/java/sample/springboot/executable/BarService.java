48c8db391f68fdbaf9886ced8221acc6f718c8b3
Olle Hallin <olle.hallin@crisp.se>
2019-11-29 23:20:42 +0100

    SampleSpringBootApp: added a couple of @Profiles

---
+++ sample/sample-spring-boot-executable-jar/src/main/java/sample/springboot/executable/BarService.java
@@ -0,0 +1,21 @@
+package sample.springboot.executable;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author olle.hallin@crisp.se
+ */
+@Component
+@Profile("bar")
+@Slf4j
+public class BarService {
+
+    @PostConstruct
+    public void postConstruct() {
+      log.info("Hello from BarService");
+    }
+}
---
+++ sample/sample-spring-boot-executable-jar/src/main/java/sample/springboot/executable/FooService.java
@@ -0,0 +1,21 @@
+package sample.springboot.executable;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author olle.hallin@crisp.se
+ */
+@Component
+@Profile("foo")
+@Slf4j
+public class FooService {
+
+    @PostConstruct
+    public void postConstruct() {
+      log.info("Hello from FooService");
+    }
+}
