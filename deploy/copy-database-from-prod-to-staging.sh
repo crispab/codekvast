e3dc1670302f048db0df5743c65ae0dd45f05629
Olle Hallin <olle.hallin@klarna.com>
2020-05-04 14:05:08 +0200

    Removed MariaDB/MySQL-related stuff from deploy/

--- deploy/copy-database-from-prod-to-staging.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#---------------------------------------------------------------------------------------------------
-# Dumps the production database to S3 and restores it to staging
-#---------------------------------------------------------------------------------------------------
-
-source $(dirname $0)/.check-requirements.sh
-
-ansible-playbook playbooks/copy-database-from-prod-to-staging.yml
--- deploy/deploy-db-to-prod.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#---------------------------------------------------------------------------------------------------
-# Deploys MariaDB to the production environment
-#---------------------------------------------------------------------------------------------------
-
-source $(dirname $0)/.check-requirements.sh
-
-ansible-playbook playbooks/database.yml --limit tag_Env_prod $*
