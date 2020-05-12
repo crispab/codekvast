86a219121a7685c703bfa2fd8074f04359dbe34b
Olle Hallin <olle.hallin@klarna.com>
2020-05-12 23:24:28 +0200

    Now staging works (except some Datadog glitches)

---
+++ deploy/docker-login.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+source $(dirname $0)/.check-requirements.sh
+
+declare region=$(grep aws_region playbooks/vars/common.yml | cut -d: -f2 | xargs)
+declare account=$(grep aws_account playbooks/vars/common.yml | cut -d: -f2 | xargs)
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.eu-central-1.amazonaws.com
