f1ca94d1028ce553b1b875c5e9040a1c78af57c4
Olle Hallin <olle.hallin@klarna.com>
2020-05-13 23:25:59 +0200

    Replaced hardcoded AWS account strings with lookup from vars/common.yml. Switched to using the same JDK in Docker as in development.

--- deploy/docker-login.sh
+++ deploy/docker-login.sh
@@ -3,4 +3,4 @@ source $(dirname $0)/.check-requirements.sh
 
 declare region=$(grep aws_region playbooks/vars/common.yml | cut -d: -f2 | xargs)
 declare account=$(grep aws_account playbooks/vars/common.yml | cut -d: -f2 | xargs)
-aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.eu-central-1.amazonaws.com
+env AWS_PROFILE=codekvast aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.eu-central-1.amazonaws.com
