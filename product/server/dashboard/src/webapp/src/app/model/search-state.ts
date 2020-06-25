98115d10e8e129bea1538bf374c58833e7076b0f
Olle Hallin <olle.hallin@klarna.com>
2020-06-25 09:18:35 +0200

    Added support for regexp in the status page.

--- product/server/dashboard/src/webapp/src/app/model/search-state.ts
+++ product/server/dashboard/src/webapp/src/app/model/search-state.ts
@@ -1,21 +1,25 @@
 export class SearchState {
-    static KEY = 'search-state';
-    environments = '';
-    applications = '';
-    locations = '';
-    hostnames = '';
-    maxResults = 100;
-    includeIfCollectedForAtLeastDays = 30;
-    includeIfNotInvokedInDays = 30;
-    includeUntrackedMethods = false;
-    includeOnlyNeverInvokedMethods = false;
+  static KEY = 'search-state';
+  environments = '';
+  applications = '';
+  versions = '';
+  locations = '';
+  hostnames = '';
+  maxResults = 100;
+  includeIfCollectedForAtLeastDays = 30;
+  includeIfNotInvokedInDays = 30;
+  includeUntrackedMethods = false;
+  includeOnlyNeverInvokedMethods = false;
 
-    public isShowingEverything() {
-        return this.isEmpty(this.applications) && this.isEmpty(this.environments) && this.isEmpty(this.locations)
-            && this.isEmpty(this.hostnames);
-    }
+  public isShowingEverything() {
+    return this.isEmpty(this.applications)
+        && this.isEmpty(this.versions)
+        && this.isEmpty(this.environments)
+        && this.isEmpty(this.locations)
+        && this.isEmpty(this.hostnames);
+  }
 
-    private isEmpty(s: string) {
-        return !s || s.trim().length === 0
-    }
+  private isEmpty(s: string) {
+    return !s || s.trim().length === 0
+  }
 }
--- product/server/dashboard/src/webapp/src/app/pages/collection-status/collection-status.component.state.ts
+++ product/server/dashboard/src/webapp/src/app/pages/collection-status/collection-status.component.state.ts
@@ -6,135 +6,139 @@ import {StateService} from '../../services/state.service';
 import {SearchState} from '../../model/search-state';
 
 export class CollectionStatusComponentState {
-    static KEY = 'collection-status';
+  static KEY = 'collection-status';
 
-    data: StatusData;
-    errorMessage: string;
-    autoRefresh = true;
-    showTerminatedAgents = false;
-    refreshIntervalSeconds = 60;
+  data: StatusData;
+  errorMessage: string;
+  autoRefresh = true;
+  showTerminatedAgents = false;
+  refreshIntervalSeconds = 60;
 
-    searchState: SearchState;
+  searchState: SearchState;
 
-    private timerSubscription: Subscription;
+  private timerSubscription: Subscription;
 
-    constructor(private agePipe: AgePipe, private api: DashboardApiService, private stateService: StateService) {
+  constructor(private agePipe: AgePipe, private api: DashboardApiService, private stateService: StateService) {
+  }
+
+  init() {
+    this.searchState = this.stateService.getState(SearchState.KEY, () => new SearchState());
+    if (this.autoRefresh) {
+      this.startAutoRefresh();
+    } else {
+      this.refreshNow();
     }
+  }
 
-    init() {
-        this.searchState = this.stateService.getState(SearchState.KEY, () => new SearchState());
-        if (this.autoRefresh) {
-            this.startAutoRefresh();
-        } else {
-            this.refreshNow();
-        }
-    }
+  destroy() {
+    this.stopAutoRefresh();
+  }
 
-    destroy() {
-        this.stopAutoRefresh();
-    }
+  autoRefreshButtonClasses() {
+    return {
+      fas: true,
+      'fa-pause': this.autoRefresh,
+      'fa-play': !this.autoRefresh
+    };
+  }
 
-    autoRefreshButtonClasses() {
-        return {
-            fas: true,
-            'fa-pause': this.autoRefresh,
-            'fa-play': !this.autoRefresh
-        };
+  toggleAutoRefresh() {
+    this.autoRefresh = !this.autoRefresh;
+    if (!this.autoRefresh) {
+      this.stopAutoRefresh();
+    } else {
+      this.startAutoRefresh();
     }
+  }
 
-    toggleAutoRefresh() {
-        this.autoRefresh = !this.autoRefresh;
-        if (!this.autoRefresh) {
-            this.stopAutoRefresh();
-        } else {
-            this.startAutoRefresh();
-        }
-    }
+  autoRefreshButtonText() {
+    return this.autoRefresh ? 'Pause auto-refresh' : 'Resume auto-refresh';
+  }
 
-    autoRefreshButtonText() {
-        return this.autoRefresh ? 'Pause auto-refresh' : 'Resume auto-refresh';
+  updateRefreshTimer() {
+    this.refreshIntervalSeconds = Math.max(10, this.refreshIntervalSeconds);
+    console.log('[ck dashboard] New refreshIntervalSeconds: %o', this.refreshIntervalSeconds);
+    if (this.autoRefresh) {
+      this.stopAutoRefresh();
+      this.startAutoRefresh();
     }
+  }
 
-    updateRefreshTimer() {
-        this.refreshIntervalSeconds = Math.max(10, this.refreshIntervalSeconds);
-        console.log('[ck dashboard] New refreshIntervalSeconds: %o', this.refreshIntervalSeconds);
-        if (this.autoRefresh) {
-            this.stopAutoRefresh();
-            this.startAutoRefresh();
-        }
-    }
+  refreshNow() {
+    this.api
+    .getStatus()
+    .subscribe(data => {
+      this.data = data;
+      this.errorMessage = undefined;
+    }, error => {
+      this.data = undefined;
+      this.errorMessage = error.statusText ? error.statusText : error;
+    });
+  }
 
-    refreshNow() {
-        this.api
-            .getStatus()
-            .subscribe(data => {
-                this.data = data;
-                this.errorMessage = undefined;
-            }, error => {
-                this.data = undefined;
-                this.errorMessage = error.statusText ? error.statusText : error;
-            });
-    }
+  communicationFailure() {
+    let now = this.agePipe.transform(new Date(), 'shortTime');
+    return now + ': Communication failure';
+  }
 
-    communicationFailure() {
-        let now = this.agePipe.transform(new Date(), 'shortTime');
-        return now + ': Communication failure';
+  getFilteredEnvironments() {
+    if (this.data.environments) {
+      let envRegExp = new RegExp(this.searchState.environments, 'i');
+      return this.data.environments.filter(e => envRegExp.test(e.name));
     }
+    return null;
+  }
 
-    getEnvironments() {
-        return this.data.environments;
+  getFilteredApplications() {
+    if (this.data.applications) {
+      let appRegExp = new RegExp(this.searchState.applications, 'i');
+      let envRegExp = new RegExp(this.searchState.environments, 'i');
+      return this.data.applications.filter(a => appRegExp.test(a.appName) && envRegExp.test(a.environment));
     }
+    return null;
+  }
 
-    getVisibleApplications() {
-        if (this.data.applications) {
-            // @formatter:off
-            return this.data.applications.filter(a =>
-                a.appName.toLowerCase().indexOf(this.searchState.applications.toLowerCase()) >= 0
-                && a.environment.toLowerCase().indexOf(this.searchState.environments.toLowerCase()) >= 0);
-            // @formatter:on
-        }
-        return null;
+  getFilteredAgents() {
+    if (this.data.agents) {
+      let appRegExp = new RegExp(this.searchState.applications, 'i');
+      let versionRegExp = new RegExp(this.searchState.versions, 'i');
+      let envRegExp = new RegExp(this.searchState.environments, 'i');
+      let hostRegExp = new RegExp(this.searchState.hostnames, 'i');
+      return this.data.agents.filter(a => appRegExp.test(a.appName)
+          && versionRegExp.test(a.appVersion)
+          && envRegExp.test(a.environment)
+          && hostRegExp.test(a.hostname));
     }
+    return null;
+  }
 
-    getFilteredAgents() {
-        if (this.data.agents) {
-            // @formatter:off
-            return this.data.agents.filter(a =>
-                `${a.appName} ${a.appVersion}`.toLowerCase().indexOf(this.searchState.applications.toLowerCase()) >= 0
-                && a.environment.toLowerCase().indexOf(this.searchState.environments.toLowerCase()) >= 0
-                && a.hostname.toLowerCase().indexOf(this.searchState.hostnames.toLowerCase()) >= 0);
-            // @formatter:on
-        }
-        return null;
-    }
+  getVisibleAgents() {
+    return this.getFilteredAgents().filter(a => (this.showTerminatedAgents || a.agentAlive));
+  }
 
-    getVisibleAgents() {
-        return this.getFilteredAgents().filter(a => (this.showTerminatedAgents || a.agentAlive));
+  numTerminatedAgents() {
+    if (this.data.agents) {
+      return this.data.agents.filter(a => !a.agentAlive).length;
     }
+    return null;
+  }
 
-    numTerminatedAgents() {
-        if (this.data.agents) {
-            return this.data.agents.filter(a => !a.agentAlive).length;
-        }
-        return null;
+  numTerminatedFilteredAgents() {
+    if (this.getFilteredAgents()) {
+      return this.getFilteredAgents().filter(a => !a.agentAlive).length;
     }
+    return null;
+  }
 
-    numTerminatedFilteredAgents() {
-        if (this.getFilteredAgents()) {
-            return this.getFilteredAgents().filter(a => !a.agentAlive).length;
-        }
-        return null;
-    }
+  private startAutoRefresh() {
+    this.timerSubscription = timer(0, this.refreshIntervalSeconds * 1000).subscribe((tick: number) => {
+      console.log('[ck dashboard] Doing auto-refresh #%o', tick);
+      this.refreshNow();
+    });
+  }
 
-    private startAutoRefresh() {
-        this.timerSubscription = timer(0, this.refreshIntervalSeconds * 1000).subscribe((tick: number) => {
-            console.log('[ck dashboard] Doing auto-refresh #%o', tick);
-            this.refreshNow();
-        });
-    }
-
-    private stopAutoRefresh() {
-        this.timerSubscription.unsubscribe();
-    }
+  private stopAutoRefresh() {
+    this.timerSubscription.unsubscribe();
+  }
 
 }
