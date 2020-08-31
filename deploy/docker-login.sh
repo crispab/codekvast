2039bc795abea14eed0579387addc97b9b189aba
Olle Hallin <olle.hallin@klarna.com>
2020-08-31 16:19:32 +0200

    Adapted docker-login.sh to AWS CLI V2

--- deploy/docker-login.sh
+++ deploy/docker-login.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 source $(dirname $0)/.check-requirements.sh
 
-eval $(aws ecr get-login --no-include-email --region ${AWS_REGION})
+declare AWS_ACCOUNT=$(yq read playbooks/vars/common.yml aws_account)
+declare AWS_REGION=$(yq read playbooks/vars/common.yml aws_region)
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.eu-central-1.amazonaws.com
