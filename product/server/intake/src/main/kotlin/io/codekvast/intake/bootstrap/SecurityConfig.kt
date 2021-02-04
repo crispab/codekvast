fde79a4bf1ebe6e035f96de0e1877d38dd1e6e9d
Olle Hallin <olle.hallin@crisp.se>
2021-02-04 22:57:09 +0100

    Added SecurityConfig.kt to CodekvastIntakeApplication

---
+++ product/server/intake/src/main/kotlin/io/codekvast/intake/bootstrap/SecurityConfig.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015-2021 Hallin Information Technology AB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.codekvast.intake.bootstrap
+
+import lombok.RequiredArgsConstructor
+import lombok.extern.slf4j.Slf4j
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+/**
+ * Configuration of Spring Security.
+ *
+ * @author olle.hallin@crisp.se
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+@Slf4j
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(httpSecurity: HttpSecurity) {
+        httpSecurity
+            .csrf()
+            .ignoringAntMatchers(
+                "/management/**",
+                "/javaagent/**"
+            )
+            .and()
+            .authorizeRequests()
+            .antMatchers(
+                "/management/**",
+                "/javaagent/**"
+            )
+            .permitAll()
+    }
+
+}
