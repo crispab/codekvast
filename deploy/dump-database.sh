19287c2913c610e2551865a4a0e0ca5dcb8399d3
Olle Hallin <olle.hallin@hit.se>
2021-11-28 00:32:02 +0100

    Switched from mysqldump to mariadb-dump

--- deploy/dump-database.sh
+++ deploy/dump-database.sh
@@ -16,8 +16,8 @@ case ${srcEnv} in
   exit 1;;
 esac
 
-if [[ -z $(which mysqldump) ]]; then
-  echo "mysqldump is not installed" >&1
+if [[ -z $(which mariadb-dump) ]]; then
+  echo "mariadb-dump is not installed" >&1
   exit 1
 fi
 
@@ -30,8 +30,8 @@ declare host=db-${srcEnv}.codekvast.io
 declare password=$(yq eval ".secrets.mariadb.${srcEnv}.root_password" playbooks/vars/secrets.yml )
 declare startedAtSecond=$(date +"%s")
 declare targetFile=${targetDirectory}/${dumpFile}
-echo "mysqldump --host=${host} --user=${username} --password=XXX --single-transaction --databases codekvast > $targetFile"
-mysqldump --host=${host} --user=${username} --password=${password} --single-transaction --databases codekvast > ${targetFile}
+echo "mariadb-dump --host=${host} --user=${username} --password=XXX --single-transaction --databases codekvast > $targetFile"
+mariadb-dump --host=${host} --user=${username} --password=${password} --single-transaction --databases codekvast > ${targetFile}
 declare elapsedSeconds=$(( $(date +"%s") - $startedAtSecond))
 declare dumpSize=$(( $(stat --format="%s" ${targetFile}) / 1000000 ))
 echo "Dumped codekvast-${srcEnv} to $targetFile ($dumpSize MB) in $elapsedSeconds seconds"
