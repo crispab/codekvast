98cf6015693e1c84c9d640d15f14fc5594a9025d
Olle Hallin <olle.hallin@hit.se>
2021-12-13 23:02:25 +0100

    Converted MethodAnalyzerTest to JUnit5

--- product/agent/java-agent/src/test/java/io/codekvast/javaagent/config/MethodAnalyzerTest.java
+++ product/agent/java-agent/src/test/java/io/codekvast/javaagent/config/MethodAnalyzerTest.java
@@ -1,115 +1,133 @@
 package io.codekvast.javaagent.config;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.fail;
 
 import io.codekvast.javaagent.util.SignatureUtils;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.Verifier;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
+import io.codekvast.junit5.extensions.CaptureSystemOutput;
+import io.codekvast.junit5.extensions.CaptureSystemOutput.OutputCapture;
+import java.util.Arrays;
+import java.util.List;
+import lombok.Builder;
+import lombok.ToString;
+import lombok.Value;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-/**
- * Test for the visibility part of MethodAnalyzer.
- *
- * @author olle.hallin@crisp.se
- */
-@SuppressWarnings("UseOfSystemOutOrSystemErr")
-@RunWith(Parameterized.class)
+@CaptureSystemOutput
 public class MethodAnalyzerTest {
 
-  private final ByteArrayOutputStream capturedSystemErr = new ByteArrayOutputStream();
+  @ParameterizedTest
+  @MethodSource("testCases")
+  void should_parseVisibility(TestCase testCase, OutputCapture outputCapture) {
+    // Given, When
+    MethodAnalyzer analyzer = new MethodAnalyzer(testCase.input);
 
-  @Parameter(0)
-  public String input;
-
-  @Parameter(1)
-  public boolean selectsPublic;
-
-  @Parameter(2)
-  public boolean selectsProtected;
-
-  @Parameter(3)
-  public boolean selectsPackagePrivate;
-
-  @Parameter(4)
-  public boolean selectsPrivate;
-
-  @Parameter(5)
-  public boolean expectsSystemErr;
-
-  @Parameter(6)
-  public String expectedToString;
-
-  @Rule
-  public Verifier verifier =
-      new Verifier() {
-        @Override
-        public void verify() throws IOException {
-          String output = capturedSystemErr.toString();
-          if (!output.isEmpty() && !expectsSystemErr) {
-            fail("Unexpected output on System.err: " + output);
-          }
-          if (output.isEmpty() && expectsSystemErr) {
-            fail("Expected output on System.err");
-          }
-        }
-      };
-
-  private PrintStream savedSystemErr;
-
-  @Parameters(name = "{index}: {0}")
-  public static Object[][] data() {
-    return new Object[][] {
-      {null, true, false, false, false, false, SignatureUtils.PUBLIC},
-      {"   ", true, false, false, false, false, SignatureUtils.PUBLIC},
-      {" foobar ", true, false, false, false, true, SignatureUtils.PUBLIC},
-      {"   ", true, false, false, false, false, SignatureUtils.PUBLIC},
-      {"public", true, false, false, false, false, SignatureUtils.PUBLIC},
-      {"PuBlIc", true, false, false, false, false, SignatureUtils.PUBLIC},
-      {" public ", true, false, false, false, false, SignatureUtils.PUBLIC},
-      {"protected", true, true, false, false, false, SignatureUtils.PROTECTED},
-      {" protected ", true, true, false, false, false, SignatureUtils.PROTECTED},
-      {" PROTECTED ", true, true, false, false, false, SignatureUtils.PROTECTED},
-      {"package-private", true, true, true, false, false, SignatureUtils.PACKAGE_PRIVATE},
-      {"!private", true, true, true, false, false, SignatureUtils.PACKAGE_PRIVATE},
-      {"private", true, true, true, true, false, SignatureUtils.PRIVATE},
-      {"all", true, true, true, true, false, SignatureUtils.PRIVATE},
-    };
-  }
-
-  @Before
-  public void beforeTest() {
-    savedSystemErr = System.err;
-    System.setErr(new PrintStream(capturedSystemErr));
-  }
-
-  @After
-  public void afterTest() {
-    System.setErr(savedSystemErr);
-    System.err.print(capturedSystemErr.toString());
-  }
-
-  @Test
-  public void shouldParseVisibility() {
-    MethodAnalyzer filter = new MethodAnalyzer(input);
-    assertThat("Should select public", filter.selectsPublicMethods(), is(selectsPublic));
-    assertThat("Should select protected", filter.selectsProtectedMethods(), is(selectsProtected));
+    // Then
+    assertThat("Should select public", analyzer.selectsPublicMethods(), is(testCase.selectsPublic));
+    assertThat(
+        "Should select protected",
+        analyzer.selectsProtectedMethods(),
+        is(testCase.selectsProtected));
     assertThat(
         "Should select package private",
-        filter.selectsPackagePrivateMethods(),
-        is(selectsPackagePrivate));
-    assertThat("Should select private", filter.selectsPrivateMethods(), is(selectsPrivate));
+        analyzer.selectsPackagePrivateMethods(),
+        is(testCase.selectsPackagePrivate));
+    assertThat(
+        "Should select private", analyzer.selectsPrivateMethods(), is(testCase.selectsPrivate));
 
-    assertThat("Should normalize toString()", filter.toString(), is(expectedToString));
+    assertThat("Should normalize toString()", analyzer.toString(), is(testCase.expectedToString));
+
+    if (testCase.expectedSystemErr != null) {
+      outputCapture.expect(containsString(testCase.expectedSystemErr));
+    } else {
+      outputCapture.expect(is(""));
+    }
+  }
+
+  static List<TestCase> testCases() {
+    return Arrays.asList(
+        TestCase.builder()
+            .input(null)
+            .selectsPublic(true)
+            .expectedToString(SignatureUtils.PUBLIC)
+            .build(),
+        TestCase.builder()
+            .input("   ")
+            .selectsPublic(true)
+            .expectedToString(SignatureUtils.PUBLIC)
+            .build(),
+        TestCase.builder()
+            .input("public")
+            .selectsPublic(true)
+            .expectedToString(SignatureUtils.PUBLIC)
+            .build(),
+        TestCase.builder()
+            .input(" public ")
+            .selectsPublic(true)
+            .expectedToString(SignatureUtils.PUBLIC)
+            .build(),
+        TestCase.builder()
+            .input("PuBlIc")
+            .selectsPublic(true)
+            .expectedToString(SignatureUtils.PUBLIC)
+            .build(),
+        TestCase.builder()
+            .input("  foobar ")
+            .selectsPublic(true)
+            .expectedToString(SignatureUtils.PUBLIC)
+            .expectedSystemErr(
+                "Unrecognized value for methodVisibility: \"foobar\", assuming \"public\"")
+            .build(),
+        TestCase.builder()
+            .input("protected")
+            .selectsPublic(true)
+            .selectsProtected(true)
+            .expectedToString(SignatureUtils.PROTECTED)
+            .build(),
+        TestCase.builder()
+            .input("package-private")
+            .selectsPublic(true)
+            .selectsProtected(true)
+            .selectsPackagePrivate(true)
+            .expectedToString(SignatureUtils.PACKAGE_PRIVATE)
+            .build(),
+        TestCase.builder()
+            .input("!private")
+            .selectsPublic(true)
+            .selectsProtected(true)
+            .selectsPackagePrivate(true)
+            .expectedToString(SignatureUtils.PACKAGE_PRIVATE)
+            .build(),
+        TestCase.builder()
+            .input("private")
+            .selectsPublic(true)
+            .selectsProtected(true)
+            .selectsPackagePrivate(true)
+            .selectsPrivate(true)
+            .expectedToString(SignatureUtils.PRIVATE)
+            .build(),
+        TestCase.builder()
+            .input("all")
+            .selectsPublic(true)
+            .selectsProtected(true)
+            .selectsPackagePrivate(true)
+            .selectsPrivate(true)
+            .expectedToString(SignatureUtils.PRIVATE)
+            .build());
+  }
+
+  @Value
+  @Builder
+  @ToString(of = "input")
+  static class TestCase {
+    String input;
+    boolean selectsPublic;
+    boolean selectsProtected;
+    boolean selectsPackagePrivate;
+    boolean selectsPrivate;
+    String expectedToString;
+    String expectedSystemErr;
   }
 }
