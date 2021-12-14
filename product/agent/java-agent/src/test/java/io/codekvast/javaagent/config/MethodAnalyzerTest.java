2debcb4ebb0f03b0dff7ae9169f894dab626be64
Olle Hallin <olle.hallin@hit.se>
2021-12-14 08:40:10 +0100

    Improved MethodAnalyzerTest.

--- product/agent/java-agent/src/test/java/io/codekvast/javaagent/config/MethodAnalyzerTest.java
+++ product/agent/java-agent/src/test/java/io/codekvast/javaagent/config/MethodAnalyzerTest.java
@@ -1,5 +1,6 @@
 package io.codekvast.javaagent.config;
 
+import static io.codekvast.javaagent.config.MethodAnalyzerTest.TestCase.ofInput;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,14 +10,14 @@ import io.codekvast.junit5.extensions.CaptureSystemOutput;
 import io.codekvast.junit5.extensions.CaptureSystemOutput.OutputCapture;
 import java.util.Arrays;
 import java.util.List;
-import lombok.Builder;
 import lombok.ToString;
 import lombok.Value;
+import lombok.With;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 @CaptureSystemOutput
-public class MethodAnalyzerTest {
+class MethodAnalyzerTest {
 
   @ParameterizedTest
   @MethodSource("testCases")
@@ -25,15 +26,18 @@ public class MethodAnalyzerTest {
     MethodAnalyzer analyzer = new MethodAnalyzer(testCase.input);
 
     // Then
-    assertThat("Should select public", analyzer.selectsPublicMethods(), is(testCase.selectsPublic));
+    assertThat("Should select public", analyzer.selectsPublicMethods(), is(true));
+
     assertThat(
         "Should select protected",
         analyzer.selectsProtectedMethods(),
         is(testCase.selectsProtected));
+
     assertThat(
         "Should select package private",
         analyzer.selectsPackagePrivateMethods(),
         is(testCase.selectsPackagePrivate));
+
     assertThat(
         "Should select private", analyzer.selectsPrivateMethods(), is(testCase.selectsPrivate));
 
@@ -48,86 +52,49 @@ public class MethodAnalyzerTest {
 
   static List<TestCase> testCases() {
     return Arrays.asList(
-        TestCase.builder()
-            .input(null)
-            .selectsPublic(true)
-            .expectedToString(SignatureUtils.PUBLIC)
-            .build(),
-        TestCase.builder()
-            .input("   ")
-            .selectsPublic(true)
-            .expectedToString(SignatureUtils.PUBLIC)
-            .build(),
-        TestCase.builder()
-            .input("public")
-            .selectsPublic(true)
-            .expectedToString(SignatureUtils.PUBLIC)
-            .build(),
-        TestCase.builder()
-            .input(" public ")
-            .selectsPublic(true)
-            .expectedToString(SignatureUtils.PUBLIC)
-            .build(),
-        TestCase.builder()
-            .input("PuBlIc")
-            .selectsPublic(true)
-            .expectedToString(SignatureUtils.PUBLIC)
-            .build(),
-        TestCase.builder()
-            .input("  foobar ")
-            .selectsPublic(true)
-            .expectedToString(SignatureUtils.PUBLIC)
-            .expectedSystemErr(
-                "Unrecognized value for methodVisibility: \"foobar\", assuming \"public\"")
-            .build(),
-        TestCase.builder()
-            .input("protected")
-            .selectsPublic(true)
-            .selectsProtected(true)
-            .expectedToString(SignatureUtils.PROTECTED)
-            .build(),
-        TestCase.builder()
-            .input("package-private")
-            .selectsPublic(true)
-            .selectsProtected(true)
-            .selectsPackagePrivate(true)
-            .expectedToString(SignatureUtils.PACKAGE_PRIVATE)
-            .build(),
-        TestCase.builder()
-            .input("!private")
-            .selectsPublic(true)
-            .selectsProtected(true)
-            .selectsPackagePrivate(true)
-            .expectedToString(SignatureUtils.PACKAGE_PRIVATE)
-            .build(),
-        TestCase.builder()
-            .input("private")
-            .selectsPublic(true)
-            .selectsProtected(true)
-            .selectsPackagePrivate(true)
-            .selectsPrivate(true)
-            .expectedToString(SignatureUtils.PRIVATE)
-            .build(),
-        TestCase.builder()
-            .input("all")
-            .selectsPublic(true)
-            .selectsProtected(true)
-            .selectsPackagePrivate(true)
-            .selectsPrivate(true)
-            .expectedToString(SignatureUtils.PRIVATE)
-            .build());
+        ofInput(null),
+        ofInput("   "),
+        ofInput("public"),
+        ofInput(" public "),
+        ofInput("PuBlIc"),
+        ofInput("  foobar ")
+            .withExpectedSystemErr(
+                "Unrecognized value for methodVisibility: \"foobar\", assuming \"public\""),
+        ofInput("protected")
+            .withSelectsProtected(true)
+            .withExpectedToString(SignatureUtils.PROTECTED),
+        ofInput("package-private")
+            .withSelectsProtected(true)
+            .withSelectsPackagePrivate(true)
+            .withExpectedToString(SignatureUtils.PACKAGE_PRIVATE),
+        ofInput("!private")
+            .withSelectsProtected(true)
+            .withSelectsPackagePrivate(true)
+            .withExpectedToString(SignatureUtils.PACKAGE_PRIVATE),
+        ofInput("private")
+            .withSelectsProtected(true)
+            .withSelectsPackagePrivate(true)
+            .withSelectsPrivate(true)
+            .withExpectedToString(SignatureUtils.PRIVATE),
+        ofInput("all")
+            .withSelectsProtected(true)
+            .withSelectsPackagePrivate(true)
+            .withSelectsPrivate(true)
+            .withExpectedToString(SignatureUtils.PRIVATE));
   }
 
   @Value
-  @Builder
   @ToString(of = "input")
   static class TestCase {
     String input;
-    boolean selectsPublic;
-    boolean selectsProtected;
-    boolean selectsPackagePrivate;
-    boolean selectsPrivate;
-    String expectedToString;
-    String expectedSystemErr;
+    @With boolean selectsProtected;
+    @With boolean selectsPackagePrivate;
+    @With boolean selectsPrivate;
+    @With String expectedToString;
+    @With String expectedSystemErr;
+
+    static TestCase ofInput(String input) {
+      return new TestCase(input, false, false, false, SignatureUtils.PUBLIC, null);
+    }
   }
 }
